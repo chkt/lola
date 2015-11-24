ff02aa551931876170c041be346bd2b25acaeeb6
chkt <ck@christoph-kettelhoit.de>
2015-11-24 19:31:32 +0000

    Added ACollectionController

---
+++ source/server/chkt/ctrl/ACollectionController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace chkt\ctrl;
+
+use chkt\ctrl\AReplyController;
+
+use chkt\route\Route;
+
+use chkt\http\HttpRequest;
+use chkt\http\HttpReply;
+
+
+
+abstract class ACollectionController extends AReplyController {
+	
+	const VERSION = '0.0.7';
+		
+	
+	
+	protected $_itemController = null;
+	
+	
+	public function __construct() {
+		$this
+			->setResolveTransform(function(Route $route) {
+				$request = $this->useRequest();
+
+				$mime = $request->getPreferedAcceptMime([
+					HttpRequest::MIME_JSON
+				]);
+
+				if (empty($mime)) return 'unavailable';
+				
+				switch($request->getMethod()) {
+					case HttpRequest::METHOD_GET : return 'read';
+					case HttpRequest::METHOD_PUT : return 'create';
+					default : return 'unavailable';
+				}
+			})
+			->setReplyTransform(function(Route $route, $reply) {
+				return json_encode($reply);
+			})
+			->useReply()
+			->setMime(HttpReply::MIME_JSON);
+	}
+	
+	
+	protected function createAction(Route $route) {
+		if (is_null($this->_itemController)) return $this->unavailableAction($route);
+		
+		$request = $this->useRequest();
+		
+		return $route
+			->setCtrl($this->_itemController, 'resolve')
+			->enter(function(&$ins, $route) use ($request) {
+				$ins->setRequest($request);
+			});
+	}
+	
+	
+	abstract protected function readAction(Route $route);
+	
+	
+	public function unavailableAction(Route $route) {
+		$this
+			->useReply()
+			->setCode(400)
+			->setMime(HttpReply::MIME_PLAIN)
+			->send();
+	}
+	
+	public function unauthenticatedAction(Route $route) {
+		$this
+			->useReply()
+			->setCode(403)
+			->setMime(HttpReply::MIME_PLAIN)
+			->send();
+	}
+}
