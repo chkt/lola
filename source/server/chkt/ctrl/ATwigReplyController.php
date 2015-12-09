01776860e7751fb6afd5130c0cdd14d0cbc4da4c
chkt <ck@christoph-kettelhoit.de>
2015-12-09 20:50:40 +0000

    Updated ATwigReplyController to use ReplyProcessor

--- source/server/chkt/ctrl/ATwigReplyController.php
+++ source/server/chkt/ctrl/ATwigReplyController.php
@@ -4,23 +4,26 @@ namespace chkt\ctrl;
 
 use chkt\ctrl\AReplyController;
 
+use chkt\route\Route;
+use chkt\http\HttpReply;
+
 
 
 abstract class ATwigReplyController extends AReplyController {
 	
-	const VERSION = '0.0.6';
+	const VERSION = '0.0.8';
 	
-	
-	protected $_twigView = '';
 	
 	
 	public function __construct($view = 'default') {
 		if (!is_string($view) || empty($view)) throw new \ErrorException();
-		
-		$this->_twigView = $view;
-		
-		$this->setReplyTransform(function($route, $reply) {
-			return $this->_useInjected('app')->drawTwigView($this->_twigView, $route->getView(), $reply);
-		});
+				
+		$this
+			->useReplyProcessor()
+			->append('view', function(Route $route, HttpReply& $reply) use ($view) {
+				$view = $this->_useInjected('app')->drawTwigView($view, $route->getView(), $route->useVars());
+
+				$reply->setContent($view);
+			});
 	}
 }
