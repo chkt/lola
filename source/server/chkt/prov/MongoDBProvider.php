03dfd632129fc1e12ebd78b2c77df40da9c0bfd8
chkt <ck@christoph-kettelhoit.de>
2016-01-03 22:41:16 +0000

    Changed MongoDBProvider to require IApp instead of App

--- source/server/chkt/prov/MongoDBProvider.php
+++ source/server/chkt/prov/MongoDBProvider.php
@@ -4,7 +4,7 @@ namespace chkt\prov;
 
 use \chkt\prov\AProvider;
 
-use \chkt\app\App;
+use \chkt\app\IApp;
 
 use \MongoDB\Client;
 
@@ -12,7 +12,7 @@ use \MongoDB\Client;
 
 class MongoDBProvider extends AProvider {
 	
-	public function __construct(App $app) {
+	public function __construct(IApp $app) {
 		parent::__construct(function($id) use ($app) {
 			$props = $app->getProperty('mongodb');
 			
