8d2b29a8e23df5908f9720847638656a402a030a
chkt <ck@christoph-kettelhoit.de>
2016-01-03 22:15:10 +0000

    Moved TAppInjector to chkt\inject, Changed

---
+++ source/server/chkt/inject/TAppInjector.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace chkt\inject;
+
+use chkt\inject\Injector;
+
+//REVIEW due to a bug in PHP we cannot use the same trait multiple times
+//Until the fix we have to magically assume the existance of methods
+//use chkt\prov\TAppLocator;
+
+
+
+trait TAppInjector {
+	//use TAppLocator
+
+
+	
+	private $_tInjector = null;
+	
+
+	public function& useInjector() {
+		if (is_null($this->_tInjector)) $this->_tInjector = new Injector($this->useLocator(), [
+			'app' =>& $this
+		]);
+		
+		return $this->_tInjector;
+	}
+}
