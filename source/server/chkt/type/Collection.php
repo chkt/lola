62dcab914d4d951c3c44e54adc5097ff62481bbf
chkt <ck@christoph-kettelhoit.de>
2016-01-03 20:19:28 +0000

    Added constructor argument, documentation

--- source/server/chkt/type/Collection.php
+++ source/server/chkt/type/Collection.php
@@ -6,19 +6,40 @@ namespace chkt\type;
 
 class Collection {
 	
+	const VERSION = '0.1.0';
+	
+	
+	
+	/**
+	 * The collection items
+	 * @var array
+	 */
 	protected $_items = null;
 	
 	
-	public function __construct() {
-		$this->_items = [];
+	/**
+	 * Creates an instance
+	 * @param array $items
+	 */
+	public function __construct(Array $items = []) {
+		$this->_items = $items;
 	}
 	
 	
+	/**
+	 * Returns true if the instance has items, false otherwise
+	 * @return bool
+	 */
 	public function hasItems() {
 		return (bool) count($this->_items);
 	}
 	
 	
+	/**
+	 * Gets the items, optionally filtered by name
+	 * @param array $names The name filter
+	 * @return array
+	 */
 	public function getItems(Array $names = null) {
 		if (is_null($names)) return $this->_items;
 		
@@ -27,6 +48,11 @@ class Collection {
 		return array_intersect_key($this->_items, $key);
 	}
 	
+	/**
+	 * Sets the items in $items
+	 * @param array $items
+	 * @return Collection
+	 */
 	public function setItems(Array $items) {
 		$this->_items = array_merge($this->_items, $items);
 		
@@ -34,6 +60,12 @@ class Collection {
 	}
 	
 	
+	/**
+	 * Returns true if the instance has item $name, false otherwise
+	 * @param string $name
+	 * @return bool
+	 * @throws \ErrorException if $name is not a nonempty string
+	 */
 	public function hasItem($name) {
 		if (!is_string($name) || empty($name)) throw new \ErrorException();
 		
@@ -41,12 +73,25 @@ class Collection {
 	}
 	
 	
+	/**
+	 * Gets item $name
+	 * @param string $name
+	 * @return mixed
+	 * @throws \ErrorException if $name is not a nonempty string
+	 */
 	public function getItem($name) {
 		if (!is_string($name) || empty($name)) throw new \ErrorException();
 		
 		return array_key_exists($name, $this->_items) ? $this->_items[$name] : null;
 	}
 	
+	/**
+	 * Sets the item $name
+	 * @param string $name
+	 * @param mixed $item
+	 * @return Collection
+	 * @throws \ErrorException if $name is not a nonempty string
+	 */
 	public function setItem($name, $item) {
 		if (!is_string($name) || empty($name)) throw new \ErrorException();
 		
@@ -56,6 +101,10 @@ class Collection {
 	}
 	
 	
+	/**
+	 * Returns a reference to the items
+	 * @return array
+	 */
 	public function& useItems() {
 		return $this->_items;
 	}
