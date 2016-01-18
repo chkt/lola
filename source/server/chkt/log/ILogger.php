1c5aff53fd4195a39eba6839c1afeeeb8f23d97e
chkt <ck@christoph-kettelhoit.de>
2016-01-18 01:31:08 +0000

    Added ->logTags(), TAG_* constants

--- source/server/chkt/log/ILogger.php
+++ source/server/chkt/log/ILogger.php
@@ -16,9 +16,37 @@ interface ILogger {
 	const IP_OR_UA = 1;
 	const IP_ALWAYS = 2;
 	
+	const TOKEN_TYPE = 'type';
+	const TOKEN_CONTENT = 'content';
+	
+	const TAG_TYPE = 1;
+	const TAG_MESSAGE = 5;
+	const TAG_VOID = 8;
+	
+	const TAG_URL_PATH = 2;
+	const TAG_SOURCE_FILE = 3;
+	const TAG_SOURCE_LINE = 4;
+	
+	const TAG_CLIENT_UA = 6;	
+	const TAG_CLIENT_IP = 7;
+	
+	const TAG_ERROR = 14;
+	const TAG_ERROR_CODE = 9;
+	const TAG_ERROR_MESSAGE = 10;
+	const TAG_ERROR_SOURCE = 13;
+	const TAG_ERROR_ARG_NAME = 15;
+	const TAG_ERROR_ARG_VAL = 16;
+	const TAG_ERROR_FILE = 11;
+	const TAG_ERROR_LINE = 12;
+	const TAG_STACK = 17;
+	const TAG_STACK_FILE = 18;
+	const TAG_STACK_LINE = 19;
+	
 	
 	
 	public function log($str);
+	
+	public function logTags(Array $str);
 	
 	public function logRequest(HttpRequest $request, $stackOffset = self::STACK_IGNORE);
 	
