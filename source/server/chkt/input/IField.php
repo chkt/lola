7e93376429b1d5d991493d8502fa33f847a08064
chkt <ck@christoph-kettelhoit.de>
2016-01-20 19:58:25 +0000

    Added IField

---
+++ source/server/chkt/input/IField.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace chkt\input;
+
+
+
+interface IField {
+	
+	public function isInitial();
+	
+	public function isChanged();
+	
+	
+	public function isEmpty();
+	
+	public function isNonEmpty();
+	
+	
+	public function isValidating();
+	
+	public function isValid();
+	
+	public function isSubmit();
+	
+	public function isMultiple();
+	
+	
+	public function getName();
+	
+	
+	public function getValue();
+	
+	public function setValue($value);
+	
+	
+	public function getValues();
+	
+	public function setValues(Array $values);
+	
+	
+	public function getData();
+	
+	
+	public function invalidate($state);
+}
