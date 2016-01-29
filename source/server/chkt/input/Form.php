4ff613e631f790b21e47693d8692d0d262064d51
chkt <ck@christoph-kettelhoit.de>
2016-01-29 22:53:30 +0000

    Added method and mime logging

--- source/server/chkt/input/Form.php
+++ source/server/chkt/input/Form.php
@@ -44,7 +44,12 @@ implements IInjectable
 		
 		$data = $request->getPayload();
 		
-		if (!is_array($data)) error_log('Unexpected form payload: ' . print_r($request->getBody(), true) . ' ' . filter_input(INPUT_SERVER, 'HTTP_USER_AGENT'));
+		if (!is_array($data)) error_log(
+			'Unexpected form payload: ' . 
+			print_r($request->getBody(), true) . ' ' .
+			$request->originMethod () . ' ' . $request->originMime () . ' ' .
+			$request->originClientUA() . ' '
+		);
 		
 		$this->_processor->validate($data);
 		
