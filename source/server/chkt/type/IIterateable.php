79955ced699a633f32e23524ce0cf38044d93e7e
chkt <ck@christoph-kettelhoit.de>
2016-02-19 15:49:36 +0000

    Added IIterateable

---
+++ source/server/chkt/type/IIterateable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace chkt\type;
+
+
+
+interface IIterateable {
+	
+	public function getIndex();
+	
+	public function getLength();
+	
+	
+	public function& useIndex($index);
+	
+	public function& useOffset($offset);
+	
+	
+	public function& useFirst();
+	
+	public function& useLast();
+	
+	public function& usePrev();
+	
+	public function& useNext();
+	
+	
+	public function& useItems();
+}
