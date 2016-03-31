39e233a8799f8dcfc5961d43112fb2c5d7e84f60
chkt <ck@christoph-kettelhoit.de>
2016-03-31 14:29:57 +0100

    Updated Validator; Added ValidationException

--- source/server/chkt/input/Validator.php
+++ source/server/chkt/input/Validator.php
@@ -2,6 +2,8 @@
 
 namespace chkt\input;
 
+use chkt\input\ValidationException;
+
 
 
 class Validator {
@@ -12,13 +14,50 @@ class Validator {
 	const STATE_VALID = 1;
 	const STATE_INVALID = 2;
 	
-	const V_DEFAULT = 0;
+	const T_EMPTY = 0;
+	const T_INVALID = 4;
 	const V_VALUES = 1;
 	const V_TRUE = 2;
 	const V_TYPE = 3;
 	
 	const TYPE_STRING_NONEMPTY = 1;
 	
+	
+	
+	static private function _validateValues($val, Array $list) {
+		return in_array($val, $list) ? $val : new ValidationException();
+	}
+	
+	static private function _validateFunction($val, Callable $fn, $compare = true) {
+		return call_user_func($fn, $val) === $compare ? $val : new ValidationException();
+	}
+	
+	static private function _validateType($val, $type) {
+		switch ($type) {
+			case self::TYPE_STRING_NONEMPTY :
+				return is_string($val) && !empty($val) ? $val : new ValidationException();
+				
+			default : throw new \ErrorException();
+		}
+	}
+	
+	
+	static private function _validateRule($value, $rule, $op) {
+		$map = [
+			self::V_TRUE => '_validateFunction',
+			self::V_TYPE => '_validateType',
+			self::V_VALUES => '_validateValues'
+		];
+		
+		if (!array_key_exists($rule, $map)) throw new \ErrorException();
+		
+		$method = $map[$rule];
+		$res = self::$method($value, $op);
+		
+		if ($res instanceof ValidationException) throw $res;
+		
+		return $res;
+	}
 	
 	
 	
@@ -38,38 +77,18 @@ class Validator {
 	
 	
 	public function validate($value, Array $rules) {
-		
-		$map = [
-			self::V_VALUES => function ($val, Array $map) {
-				if (in_array($val, $map)) return $val;
-				
-				throw new ValidatorException();
-			},
-			self::V_TRUE => function($val, Callable $fn) {
-				if (call_user_func($fn, $val) === true) return $val;
-				
-				throw new ValidatorException();
-			},
-			self::V_TYPE => function($val, $type) {
-				switch ($type) {
-					case self::TYPE_STRING_NONEMPTY :
-						if (is_string($val) && !empty($val)) return $val;
-						
-						break;
-				}
-				
-				throw new ValidatorException();
-			}
-		];
-		
 		foreach ($rules as $rule => $op) {
 			try {
-				$value = $map[$rule]($op, $value);
-			} catch (\Exception $ex) {
-				$this->_state = self::STATE_INVALID;
-				$this->_exception = $ex;
+				$value = self::_validateRule($value, $rule, $op);
+			}
+			catch (ValidationException $ex) {
+				if (array_key_exists(self::V_DEFAULT_INVALID, $rules)) $value = $rules[self::V_DEFAULT_INVALID];
+				else {
+					$this->_state = self::STATE_INVALID;
+					$this->_exception = $ex;
+				}
 				
-				$value = null;
+				break;
 			}
 		}
 		
@@ -82,9 +101,6 @@ class Validator {
 	}
 	
 	public function validateProperties(Array $source, Array $props) {
-		foreach ($props as $items) {
-			
-		}
 	}
 	
 	
