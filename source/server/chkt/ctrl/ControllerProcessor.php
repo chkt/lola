134d65ca7fb9daf3b0cdba81915a55924584e2e0
chkt <ck@christoph-kettelhoit.de>
2016-03-31 14:28:46 +0100

    Added ->getQueuedNames(), ->skip(), ->skipAll()

--- source/server/chkt/ctrl/ControllerProcessor.php
+++ source/server/chkt/ctrl/ControllerProcessor.php
@@ -10,8 +10,64 @@ class ControllerProcessor
 extends NamedQueue
 {	
 	
+	const VERSION = '0.1.6';
+	
+	
+	
+	private $_qkeys = null;
+	private $_qcbs = null;
+	
+	
+	public function __construct(array $callbacks = null) {
+		parent::__construct($callbacks);
+		
+		$this->_qkeys = [];
+		$this->_qcbs = [];
+	}
+	
+	
+	public function getQueuedNames() {
+		return $this->_qkeys;
+	}
+	
+	
 	public function process(AReplyController& $ctrl) {
-		foreach ($this->_cbs as $cb) call_user_func($cb, $ctrl);
+		$this->_qkeys = array_keys($this->_cbs);
+		$this->_qcbs = array_values($this->_cbs);
+		
+		$keys =& $this->_qkeys;
+		$cbs =& $this->_qcbs;
+		
+		while (count($cbs) !== 0) {
+			array_shift($keys);
+			$cb = array_shift($cbs);
+			
+			call_user_func($cb, $ctrl);
+		}
+		
+		return $this;
+	}
+	
+	
+	public function skip($name) {
+		if (!is_string($name) || empty($name)) throw new \ErrorException();
+		
+		$keys =& $this->_qkeys;
+		
+		$index = array_search($name, $keys);
+		
+		if ($index === false) throw new \ErrorException();
+		
+		array_splice($keys, $index, 1);
+		array_splice($this->_qcbs, $index, 1);
+		
+		return $this;
+	}
+	
+	
+	public function skipAll() {
+		$this->_qkeys = [];
+		$this->_qcbs = [];
 		
 		return $this;
 	}
