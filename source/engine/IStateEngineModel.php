53660ae8c609f4ceb0bed9dd00ffb3d68f9d0f87
chkt <ck@christoph-kettelhoit.de>
2016-06-06 20:55:53 +0200

    Added IStateEngineModel

---
+++ source/engine/IStateEngineModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lola\engine;
+
+use lola\model\IModel;
+
+
+
+interface IStateEngineModel
+extends IModel
+{	
+	
+	public function getState();
+	
+	public function setState($state);
+}
