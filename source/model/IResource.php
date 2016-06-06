584773a6009cdaf1938e00ddf6e2efe50de16772
chkt <ck@christoph-kettelhoit.de>
2016-06-06 19:57:51 +0200

    Moved resource constants into Interface Added ->wasCreated(), ->wasRead(), ->wasUpdated(), ->wasDelete()

--- source/model/IResource.php
+++ source/model/IResource.php
@@ -8,9 +8,32 @@ use lola\model\IResourceQuery;
 
 interface IResource {
 	
+	const INTERFACE_VERSION = '0.2.1';
+	
+	
+	const STATE_NEW = 1;
+	const STATE_LIVE = 2;
+	const STATE_DEAD = 3;
+	
+	const OP_NONE = 0x0;
+	const OP_CREATE = 0x4;
+	const OP_READ = 0x8;
+	const OP_UPDATE = 0x10;
+	const OP_DELETE = 0x20;
+	
+	
 	public function isLive();
 	
 	public function isDirty();
+	
+	
+	public function wasCreated();
+	
+	public function wasRead();
+	
+	public function wasUpdated();
+	
+	public function wasDeleted();
 	
 	
 	public function getData();
