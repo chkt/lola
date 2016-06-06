9aeebb113e089d914826baa535f937c4571eed3d
chkt <ck@christoph-kettelhoit.de>
2016-06-06 20:54:01 +0200

    Changed ModelInterceptor to require a ModelInterceptResolver instead of accepting callbacks

--- source/model/ModelInterceptor.php
+++ source/model/ModelInterceptor.php
@@ -2,30 +2,37 @@
 
 namespace lola\model;
 
-use lola\model\ModelInterceptorQueue;
+use lola\model\ModelActionQueue;
 
 
 
 class ModelInterceptor {
 	
-	const VERSION = '0.1.7';
+	const VERSION = '0.2.1';
 	
 	
 	
+	private $_link = null;
 	private $_get = null;
 	private $_update = null;
 	private $_delete = null;
 	
+	private $_resolvers = null;
+	
 	
 	public function __construct(
+		callable $link,
 		callable $get,
-		ModelInterceptorQueue& $add,
-		ModelInterceptorQueue& $remove
+		ModelActionQueue& $update,
+		ModelActionQueue& $remove
 	) {
+		$this->_link = $link;
 		$this->_get = $get;
 		
-		$this->_update =& $add;
+		$this->_update =& $update;
 		$this->_delete =& $remove;
+		
+		$this->_resolvers = [];
 	}
 	
 	
@@ -34,35 +41,32 @@ class ModelInterceptor {
 	}
 	
 	
-	public function hasUpdateCallback(callable $cb) {
-		return $this->_update->has($cb);
+	public function hasResolver(IModelInterceptResolver& $resolver) {
+		return in_array($resolver, $this->_resolvers);
 	}
 	
-	public function addUpdateCallback(callable $cb) {
-		$this->_update->append($cb);
+	public function addResolver(IModelInterceptResolver& $resolver) {
+		if (in_array($resolver, $this->_resolvers)) throw new \ErrorException();
+		
+		$this->_update->add([$resolver, 'update'], ModelActionQueue::PRIO_INTERCEPT);
+		$this->_delete->add([$resolver, 'delete'], ModelActionQueue::PRIO_INTERCEPT);
+		
+		$this->_resolvers[] = $resolver;
+		
+		call_user_func($this->_link, $resolver);
 		
 		return $this;
 	}
 	
-	public function removeUpdateCallback(callable $cb) {
-		$this->_update->remove($cb);
+	public function removeResolver(IModelInterceptResolver& $resolver) {
+		$index = array_search($resolver, $this->_resolvers);
 		
-		return $this;
-	}
-	
-	
-	public function hasDeleteCallback(callable $cb) {
-		return $this->_delete->has($cb);
-	}
-	
-	public function addDeleteCallback(callable $cb) {
-		$this->_delete->append($cb);
+		if ($index === false) throw new \ErrorException();
 		
-		return $this;
-	}
-	
-	public function removeDeleteCallback(callable $cb) {
-		$this->_delete->remove($cb);
+		$this->_update->remove([$resolver, 'update']);
+		$this->_delete->remove([$resolver, 'delete']);
+		
+		unset($this->_resolvers[$index]);
 		
 		return $this;
 	}
