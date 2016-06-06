f1a2fd664716b076964ecb83b0a1be4d126cc249
chkt <ck@christoph-kettelhoit.de>
2016-06-06 20:46:21 +0200

    Added ->wasCreated(), ->wasRead()

--- source/model/IModel.php
+++ source/model/IModel.php
@@ -7,7 +7,17 @@ namespace lola\model;
 interface IModel
 {
 	
+	const INTERFACE_VERSION = '0.2.1';
+	
+	
+	
 	public function isLive();
+	
+	
+	public function wasCreated();
+	
+	public function wasRead();
+	
 	
 	public function deferUpdates();
 	
