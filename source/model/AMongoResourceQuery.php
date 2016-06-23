6674e50aea0cdd07ae533b7f69bf57cc926c5dcf
chkt <ck@christoph-kettelhoit.de>
2016-06-23 15:27:05 +0200

    Added sorting query construction

--- source/model/AMongoResourceQuery.php
+++ source/model/AMongoResourceQuery.php
@@ -10,34 +10,64 @@ abstract class AMongoResourceQuery
 implements IResourceQuery
 {
 	
-	const VERSION = '0.1.4';
+	const VERSION = '0.2.3';
 	
 	const MODE_NONE = 0;
 	const MODE_MATCHING = 1;
 	const MODE_AGGREGATION = 2;
 	
+	const ORDER_ASC = 1;
+	const ORDER_DESC = -1;
+	
 	
 	
 	private $_require = null;
+	private $_order = null;
 	
 	protected $_query = null;
 	protected $_queryMode = 0;
+	protected $_sorting = null;
 	
 	
-	public function __construct(Array $require) {
+	public function __construct(array $require, array $sort = []) {
 		$this->_require = $require;
+		$this->_order = $sort;
 		
 		$this->_query = null;
 		$this->_queryMode = self::MODE_MATCHING;
+		$this->_sorting = null;
 	}
 	
 	
-	abstract protected function _buildQuery(Array $require, Array& $match, Array& $aggregate);
+	protected function _getSortingDirection($dir) {
+		$map = [
+			self::ORDER_ASC => 1,
+			self::ORDER_DESC => -1
+		];
+		
+		if (!in_array($dir, $map)) throw new \ErrorException();
+		
+		return $map[$dir];
+	}
+	
+	
+	protected function _buildQuery(Array $require, Array& $match, Array& $aggregate) {
+		return null;
+	}
+	
+	
+	protected function _buildSorting(array $order, array& $sorting) {
+		$sorting['_id'] = self::ORDER_ASC;
+	}
 	
 	
 		
 	public function getRequirements() {
 		return $this->_require;
+	}
+	
+	public function getOrder() {
+		return $this->_order;
 	}
 	
 	
@@ -77,5 +107,15 @@ implements IResourceQuery
 		}
 		
 		return $this->_query;
+	}
+	
+	
+	public function getSorting() {
+		if (is_null($this->_sorting)) {
+			$this->_sorting = [];
+			$this->_buildSorting($this->_order, $this->_sorting);
+		}
+		
+		return $this->_sorting;
 	}
 }
