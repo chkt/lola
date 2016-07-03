15344a8c8afad4801dc7ac1cea353bd3f4910e03
chkt <ck@christoph-kettelhoit.de>
2016-07-03 11:19:40 +0200

    Changed ->process() to keep external api stable

--- source/model/ProxyResourceQueue.php
+++ source/model/ProxyResourceQueue.php
@@ -4,18 +4,20 @@ namespace lola\model;
 
 use lola\type\AQueue;
 
+use lola\type\StructuredData;
+
 
 
 final class ProxyResourceQueue
 extends AQueue
 {
 	
-	const VERSION = '0.1.8';
+	const VERSION = '0.2.4';
 	
 	
 	
-	public function process(array $data = null) {
-		foreach ($this->_items as $cb) call_user_func($cb, ($clone = $data));
+	public function process(StructuredData $data = null) {		
+		foreach ($this->_items as $cb) call_user_func($cb, ($clone = $data->toArray()));		//LEGACY should send structured data to consumers
 		
 		return $this;
 	}
