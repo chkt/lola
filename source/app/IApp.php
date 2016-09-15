f7e81b82d8408372aec16384ad753555f42b8fec
chkt <ck@christoph-kettelhoit.de>
2016-09-15 12:54:53 +0200

    Added ->useRegistry()

--- source/app/IApp.php
+++ source/app/IApp.php
@@ -5,12 +5,14 @@ namespace lola\app;
 
 interface IApp {
 	public function __construct(Array $config);
-	
-	
+
+
 	public function& useInjector();
-	
+
 	public function& useLocator();
-	
-	
+
+	public function& useRegistry();
+
+
 	public function getProperty($name);
 }
