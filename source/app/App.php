1c7ffba45376f7640cbb8b536d20b577f2958ce9
chkt <ck@christoph-kettelhoit.de>
2016-09-15 12:55:36 +0200

    Added TAppRegistry

--- source/app/App.php
+++ source/app/App.php
@@ -6,6 +6,7 @@ use \lola\app\TAppBase;
 use \lola\app\TAppFile;
 use \lola\inject\TAppInjector;
 use \lola\prov\TAppLocator;
+use \lola\module\TAppRegistry;
 
 
 
@@ -14,28 +15,29 @@ class App implements IApp {
 	use TAppFile;
 	use TAppInjector;
 	use TAppLocator;
-	
-	
-	
-	const VERSION = '0.1.0';
-	
-	
-	
+	use TAppRegistry;
+
+
+
+	const VERSION = '0.3.0';
+
+
+
 	protected $_dict = [];
-	
-	
+
+
 	public function __construct(Array $config) {
 		$this->_dict = $config;
-		
+
 		date_default_timezone_set(array_key_exists('timezone', $config) ? $config['timezone'] : 'UTC');
-		
+
 		ob_start();
 	}
-	
-	
+
+
 	public function getProperty($name) {
 		if (!is_string($name) || empty($name) || !array_key_exists($name, $this->_dict)) throw new \ErrorException();
-		
+
 		return $this->_dict[$name];
 	}
 }
