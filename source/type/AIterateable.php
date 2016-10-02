f250fa5c890b8f7fafe3c448fc4cdd1cccfa3b57
chkt <ck@christoph-kettelhoit.de>
2016-10-02 19:34:56 +0200

    Added phpDoc

--- source/type/AIterateable.php
+++ source/type/AIterateable.php
@@ -6,19 +6,40 @@ use lola\type\IIterateable;
 
 
 
+/**
+ * Abstract iterateable
+ */
 abstract class AIterateable
 implements IIterateable
 {
+	/**
+	 * The version string
+	 */
 	const VERSION = '0.2.1';
 	
 	
 	
+	/**
+	 * The iterateable items
+	 * @var array
+	 */
 	protected $_items = null;
 	
+	/**
+	 * The iteration cursor position
+	 * @var int
+	 */
 	protected $_cursor = 0;
+	/**
+	 * The iterateable length
+	 * @var type 
+	 */
 	protected $_length = 0;
 	
 	
+	/**
+	 * Creates a new instance
+	 */
 	public function __construct() {
 		$this->_items = [];
 		
@@ -27,20 +48,39 @@ implements IIterateable
 	}
 	
 	
+	/**
+	 * Returns a reference to the item at $index
+	 * @param int $index - The item index
+	 * @return mixed
+	 */
 	protected function& _useItem($index) {
 		return $this->_items[$index];
 	}
 	
 	
+	/**
+	 * Returns the current cursor position
+	 * @return int
+	 */
 	public function getIndex() {
 		return $this->_cursor;
 	}
 	
+	/**
+	 * Returns the current iterateable length
+	 * @return int
+	 */
 	public function getLength() {
 		return $this->_length;
 	}
 	
 	
+	/**
+	 * Returns a reference to the item at $index
+	 * @param int $index - The iteration index
+	 * @return mixed
+	 * @throws \ErrorException - if $index is not a positive integer
+	 */
 	public function& useIndex($index) {
 		if (!is_int($index) || $index < 0) throw new \ErrorException();
 		
@@ -55,6 +95,12 @@ implements IIterateable
 		return $this->_useItem($index);
 	}
 	
+	/**
+	 * Returns a reference to the item relative to the current iteration position by $offset
+	 * @param int $offset - The offset to the iteration index
+	 * @return mixed
+	 * @throws \ErrorException - if $offset is not an integer
+	 */
 	public function& useOffset($offset) {
 		if (!is_int($offset)) throw new \ErrorException();
 		
@@ -72,18 +118,34 @@ implements IIterateable
 	}
 	
 	
+	/**
+	 * Returns a reference to the first item
+	 * @return mixed
+	 */
 	public function& useFirst() {
 		return $this->useIndex(0);
 	}
 	
+	/**
+	 * Returns a reference to the previous item relative to the iteration index
+	 * @return mixed
+	 */
 	public function& usePrev() {
 		return $this->useOffset(-1);
 	}
 	
+	/**
+	 * Returns a reference to the next item relative to the iteration index
+	 * @return mixed
+	 */
 	public function& useNext() {
 		return $this->useOffset(1);
 	}
 	
+	/**
+	 * Return a reference to the last item
+	 * @return mixed
+	 */
 	public function& useLast() {
 		return $this->useIndex($this->_length - 1);
 	}
