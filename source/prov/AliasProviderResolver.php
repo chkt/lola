537cf170fee1a9cddb0716360538a778ab06ccbd
chkt <ck@christoph-kettelhoit.de>
2016-10-10 12:54:42 +0200

    Updated resolvers to reflect changes in IProviderResolver

--- source/prov/AliasProviderResolver.php
+++ source/prov/AliasProviderResolver.php
@@ -6,69 +6,32 @@ use lola\prov\SimpleProviderResolver;
 
 
 
-class AliasProviderResolver extends SimpleProviderResolver {
+class AliasProviderResolver
+extends SimpleProviderResolver
+{
 	
-	const VERSION = '0.0.7';
-	
-	
-	
-	private $_alias = null;
+	const VERSION = '0.3.2';
 	
 	
-	public function __construct($default = SimpleProviderResolver::DEFAULT_ID) {
-		parent::__construct($default);
-		
-		$this->_alias = [];
-	}
 	
-	
-	private function _isCircularRef($source, $target) {
-		$alias = $this->_alias;
-		
-		if ($source === $target) return true;
-		
-		for (; array_key_exists($target, $alias); $target = $alias[$target]) {
-			if ($source === $alias[$target]) return true;
-		}
-		
-		return false;
-	}
-	
-	private function _getTargetId($id) {
-		$alias = $this->_alias;
-		
-		while (array_key_exists($id, $alias)) $id = $alias[$id];
-		
-		return $id;
-	}
-	
-	
-	public function isAlias($id) {
-		if (!is_string($id) || empty($id)) throw new \ErrorException();
-		
-		return array_key_exists($id, $this->_alias);
-	}
-	
+	/**
+	 * Creates an alias between $id and $target
+	 * @param string $id The new id
+	 * @param string $target The existing id
+	 * @return SimpleProviderResolver
+	 * @throws \ErrorException if $id is not a nonempty string
+	 * @throws \ErrorException if $target id not a nonempty string
+	 * @throws \ErrorException if $target is not an existing reference
+	 */
 	public function setAlias($id, $target) {
 		if (
 			!is_string($id) || empty($id) ||
 			!is_string($target) || empty($target) ||
-			$this->_isCircularRef($id, $target)
+			!array_key_exists($target, $this->_map)
 		) throw new \ErrorException();
 		
-		$this->_alias[$id] = $target;
+		$this->_map[$id] = $this->_map[$target];
 		
 		return $this;
-	}
-	
-	
-	public function& resolve($id, Array& $instances, Callable $factory) {
-		if (!is_string($id)) throw new \ErrorException();
-		
-		if (empty($id)) $id = $this->_default;
-		
-		if (array_key_exists($id, $this->_alias)) $id = $this->_getTargetId($id);
-		
-		return parent::resolve($id, $instances, $factory);
 	}
 }
--- source/prov/StackProviderResolver.php
+++ source/prov/StackProviderResolver.php
@@ -6,52 +6,76 @@ use lola\prov\IProviderResolver;
 
 
 
-class StackProviderResolver implements IProviderResolver {
+class StackProviderResolver
+implements IProviderResolver
+{
 
-	const VERSION = '0.1.0';
+	const VERSION = '0.3.2';
 
 	const RESOLVE_SINGLETON = 0;
 	const RESOLVE_UNIQUE = PHP_INT_MAX;
 
 
 
-	private $_default = 0;
+	/**
+	 * The id and index stack map
+	 * @var type 
+	 */
+	protected $_stack = null;
+	/**
+	 * The next free index
+	 * @var type 
+	 */
+	protected $_next = 0;
+	
+	/**
+	 * The default stack location
+	 * @var type 
+	 */
+	protected $_default = 0;
 
 
+	/**
+	 * @param int $default The default stack location
+	 * @throws \ErrorException if $default is not an int > 0
+	 */
 	public function __construct($default = self::RESOLVE_SINGLETON) {
 		if (!is_int($default) || $default < 0) throw new \ErrorException();
 
+		$this->_stack = [];
+		$this->_next = 0;
+		
 		$this->_default = $default;
 	}
 
 
-	public function& resolve($hash, Array& $instances, Callable $factory) {
+	/**
+	 * Returns the index associated with $hash
+	 * @param string $hash
+	 * @return int
+	 * @throws \ErrorException if $hash is not a nonempty string
+	 */
+	public function& resolve($hash) {
 		if (!is_string($hash) || empty($hash)) throw new \ErrorException();
 
-		$segs = explode('@', $hash);
+		$segs = explode('?', $hash);
 		$num = count($segs);
-
+		
 		if ($num > 2) throw new \ErrorException();
-
+		
 		$id = $segs[0];
-		$pos = $this->_default;
-
+		$index = $this->_default;
+		
 		if ($num === 2) {
 			if (!is_numeric($segs[1])) throw new \ErrorException();
-
-			$pos = (int) $segs[1];
+			
+			$index = (int) $segs[1];
 		}
-
-		if (!array_key_exists($id, $instances)) $instances[$id] = [];
-
-		$stack =& $instances[$id];
-		$len = count($stack);
-
-		if ($pos > $len - 1) {
-			$pos = $len;
-			$stack[] = call_user_func($factory, $id);
-		}
-
-		return $stack[$pos];
+		
+		if (!array_key_exists($id, $this->_stack)) $this->_stack[$id] = [];
+		
+		if (!array_key_exists($index, $this->_stack[$id])) $this->_stack[$id][$index] = $this->_next++;
+		
+		return $this->_stack[$id][$index];
 	}
 }
