fd659938935bdffbf8dd48a8059fff48dcc82a89
chkt <ck@christoph-kettelhoit.de>
2016-10-15 15:07:08 +0200

    Improved ->_reenter()

--- source/ctrl/AController.php
+++ source/ctrl/AController.php
@@ -16,7 +16,7 @@ implements IInjectable
 	/**
 	 * The version string
 	 */
-	const VERSION = '0.1.3';
+	const VERSION = '0.3.3';
 	
 	
 	/**
@@ -58,36 +58,39 @@ implements IInjectable
 	}
 	
 	
+	
 	/**
-	 * Reenters the instance through <code>$action</code>
+	 * Reenters the instance through $action
 	 * @param string $action The controller action
-	 * @param Route& $route The associated route
+	 * @param Route $route The associated route
 	 * @return mixed
-	 * @throws \ErrorException if <code>$action</code> is not a <em>nonempty</em> <code>String</code>
+	 * @throws \ErrorException if $action is not a nonempty string
+	 * @throws \ErrorException if $action does not reference an action of the instance
 	 */
 	protected function _reenter($action, Route& $route) {
 		if (!is_string($action) || empty($action)) throw new \ErrorException();
 		
-		if (!method_exists($this, $action . 'Action')) $action = 'default';
+		$method = $action . 'Action';
+		
+		if (!method_exists($this, $method)) throw new \ErrorException();
 		
 		$route->setAction($action);
 		
-		return call_user_func([$this, $action . 'Action'], $route);
+		return $this->$method($route);
 	}
 	
 	
 	/**
 	 * Enters the action of the instance referenced by $route
-	 * @param Route& $route The associated route
+	 * @param Route $route The associated route
 	 * @return mixed
-	 * @throws ErrorException if <code>$action</code> is not a <em>nonempty</em> <code>String</code>
 	 */
-	public function enter(Route& $route) {
-		$action = $route->getAction() . 'Action';
+	public function enter(Route& $route) {		
+		$method = $route->getAction() . 'Action';
 		
-		if (!method_exists($this, $action)) $action = 'defaultAction';
+		if (!method_exists($this, $method)) $method = 'defaultAction';
 		
-		return $this->$action($route);
+		return $this->$method($route);
 	}
 	
 	
