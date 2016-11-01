1d1f7146bb4ffdc3302340e8757fede89c6aaae2
chkt <ck@christoph-kettelhoit.de>
2016-11-01 20:29:04 +0100

    Add replyResource, replyTransform accessors

--- source/io/http/IHttpDriver.php
+++ source/io/http/IHttpDriver.php
@@ -4,9 +4,11 @@ namespace lola\io\http;
 
 use lola\io\IRequestReplyDriver;
 
+use lola\type\IStateTransform;
 use lola\io\http\IHttpCookies;
-use lola\io\http\IHttpRequestResource;
 use lola\io\http\IHttpConfig;
+use lola\io\http\IHttpRequestResource;
+use lola\io\http\IHttpReplyResource;
 
 
 
@@ -16,7 +18,23 @@ extends IRequestReplyDriver
 
 	public function& useCookies() : IHttpCookies;
 
-	public function& useRequestResource() : IHttpRequestResource;
 
 	public function& useConfig() : IHttpConfig;
+
+	public function setConfig(IHttpConfig& $config) : IHttpDriver;
+
+
+	public function& useRequestResource() : IHttpRequestResource;
+
+	public function setRequestResource(IHttpRequestResource& $resource) : IHttpDriver;
+
+
+	public function& useReplyResource() : IHttpReplyResource;
+
+	public function setReplyResource(IHttpReplyResource& $resource) : IHttpDriver;
+
+
+	public function& useReplyTransform() : IStateTransform;
+
+	public function setReplyTransform(IStateTransform& $transform) : IHttpDriver;
 }
