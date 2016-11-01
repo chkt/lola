cce2afcbb5b7aa26dd96f1bc98d8d4dc79371294
chkt <ck@christoph-kettelhoit.de>
2016-11-01 17:01:36 +0100

    Rename IHttpRequestQuery to IHttpRequestResource

--- source/io/http/IHttpDriver.php
+++ source/io/http/IHttpDriver.php
@@ -5,7 +5,7 @@ namespace lola\io\http;
 use lola\io\IRequestReplyDriver;
 
 use lola\io\http\IHttpCookies;
-use lola\io\http\IHttpRequestQuery;
+use lola\io\http\IHttpRequestResource;
 use lola\io\http\IHttpConfig;
 
 
@@ -16,7 +16,7 @@ extends IRequestReplyDriver
 
 	public function& useCookies() : IHttpCookies;
 
-	public function& useRequestQuery() : IHttpRequestQuery;
+	public function& useRequestResource() : IHttpRequestResource;
 
 	public function& useConfig() : IHttpConfig;
 }
