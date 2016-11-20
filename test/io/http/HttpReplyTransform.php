cfcdc27fbd2030ee16057d7e991a559d0363e2ba
chkt <ck@christoph-kettelhoit.de>
2016-11-20 17:00:15 +0800

    Add HttpReplyTransform mock

---
+++ test/io/http/HttpReplyTransform.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace test\io\http;
+
+use lola\io\http\IHttpDriver;
+
+
+
+class HttpReplyTransform
+extends \lola\io\http\HttpReplyTransform
+{
+		public function exitStep(IHttpDriver& $driver) {}
+}
