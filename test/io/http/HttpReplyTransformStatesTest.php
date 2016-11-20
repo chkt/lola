42a8231c8e1ca88c69f6c5e0e96d72c1b7a80e01
chkt <ck@christoph-kettelhoit.de>
2016-11-20 16:57:30 +0800

    Add HttpReplyTransformStatesTest

---
+++ test/io/http/HttpReplyTransformStatesTest.php
@@ -0,0 +1,195 @@
+<?php
+
+require_once('MockDriver.php');
+require_once('HttpReplyTransform.php');
+
+use PHPUnit\Framework\TestCase;
+use test\io\http\MockDriver;
+use test\io\http\HttpReplyTransform;
+
+
+
+class HttpReplyTransformStatesTest
+extends TestCase
+{
+
+	public function testSimpleReply() {
+		$trn = new HttpReplyTransform();
+		$driver = new MockDriver();
+		$reply =& $driver->useReply();
+
+		$reply
+			->setCode('404')
+			->setMime('text/plain')
+			->setHeader('X-Header-1', 'foo')
+			->setBody('Does not exist - sorry...');
+
+		$trn
+			->setTarget($driver)
+			->process();
+
+		$queue = $driver
+			->useReplyResource()
+			->popQueue();
+
+		$this->assertEquals($queue, [[
+			'type' => 'header',
+			'content' => 'HTTP/1.1 404 Not Found'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Type: text/plain;charset=utf-8'
+		], [
+			'type' => 'header',
+			'content' => 'X-Header-1: foo'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Length: 25'
+		], [
+			'type' => 'body',
+			'content' => 'Does not exist - sorry...'
+		]]);
+	}
+
+	public function testRedirectReply() {
+		$trn = new HttpReplyTransform();
+		$driver = new MockDriver();
+		$reply =& $driver->useReply();
+
+		$reply
+			->setCode('302')
+			->setRedirectTarget('/path/to/resource')
+			->setMime('text/plain')
+			->setHeader('X-Header-1', 'foo');
+
+		$trn
+			->setTarget($driver)
+			->process();
+
+		$queue = $driver
+			->useReplyResource()
+			->popQueue();
+
+		$this->assertEquals($queue, [[
+			'type' => 'header',
+			'content' => 'HTTP/1.1 302 Found'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Type: text/plain;charset=utf-8'
+		], [
+			'type' => 'header',
+			'content' => 'X-Header-1: foo'
+		], [
+			'type' => 'header',
+			'content' => 'Location: /path/to/resource'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Length: 30'
+		], [
+			'type' => 'body',
+			'content' => '302 - Found: /path/to/resource'
+		]]);
+	}
+
+	public function testCookieReply() {
+		$trn = new HttpReplyTransform();
+		$driver = new MockDriver();
+		$reply =& $driver->useReply();
+
+		$reply
+			->setCode('404')
+			->setMime('text/plain')
+			->setHeader('X-Header-1', 'foo')
+			->setBody('Does not exist - sorry...')
+			->useCookies()
+			->set('a', 'bar', 1234)
+			->set('b', 'baz', 4321);
+
+		$trn
+			->setTarget($driver)
+			->process();
+
+		$queue = $driver
+			->useReplyResource()
+			->popQueue();
+
+		$this->assertEquals($queue, [[
+			'type' => 'header',
+			'content' => 'HTTP/1.1 404 Not Found'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Type: text/plain;charset=utf-8'
+		], [
+			'type' => 'header',
+			'content' => 'X-Header-1: foo'
+		], [
+			'type' => 'cookie',
+			'name' => 'a',
+			'value' => 'bar',
+			'expires' => 1234
+		], [
+			'type' => 'cookie',
+			'name' => 'b',
+			'value' => 'baz',
+			'expires' => 4321
+		], [
+			'type' => 'header',
+			'content' => 'Content-Length: 25'
+		], [
+			'type' => 'body',
+			'content' => 'Does not exist - sorry...'
+		]]);
+	}
+
+	public function testCookieRedirectReply() {
+		$trn = new HttpReplyTransform();
+		$driver = new MockDriver();
+		$reply =& $driver->useReply();
+
+		$reply
+			->setCode('302')
+			->setRedirectTarget('/path/to/resource')
+			->setMime('text/plain')
+			->setHeader('X-Header-1', 'foo')
+			->useCookies()
+			->set('a', 'bar', 1234)
+			->set('b', 'baz', 4321);
+
+		$trn
+			->setTarget($driver)
+			->process();
+
+		$queue = $driver
+			->useReplyResource()
+			->popQueue();
+
+		$this->assertEquals($queue, [[
+			'type' => 'header',
+			'content' => 'HTTP/1.1 302 Found'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Type: text/plain;charset=utf-8',
+		], [
+			'type' => 'header',
+			'content' => 'X-Header-1: foo'
+		], [
+			'type' => 'cookie',
+			'name' => 'a',
+			'value' => 'bar',
+			'expires' => 1234
+		], [
+			'type' => 'cookie',
+			'name' => 'b',
+			'value' => 'baz',
+			'expires' => 4321
+		], [
+			'type' => 'header',
+			'content' => 'Location: /path/to/resource'
+		], [
+			'type' => 'header',
+			'content' => 'Content-Length: 30'
+		], [
+			'type' => 'body',
+			'content' => '302 - Found: /path/to/resource'
+		]]);
+	}
+}
