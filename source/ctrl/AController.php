b1e0ad83c0e3246e0192c267af66c079d6d9610a
chkt <ck@christoph-kettelhoit.de>
2016-11-24 13:01:59 +0800

    Add ->hasAction(), ->isEnterable()

--- source/ctrl/AController.php
+++ source/ctrl/AController.php
@@ -12,13 +12,13 @@ use lola\route\RouteCanceledException;
 abstract class AController
 implements IInjectable
 {
-	
+
 	/**
 	 * The version string
 	 */
-	const VERSION = '0.3.3';
-	
-	
+	const VERSION = '0.5.0';
+
+
 	/**
 	 * Gets the dependency configuration
 	 * @param array $config The seed config
@@ -27,8 +27,8 @@ implements IInjectable
 	static public function getDependencyConfig(Array $config) {
 		return [];
 	}
-	
-	
+
+
 	/**
 	 * Returns a camelCase representation of <code>$path</code>
 	 * @param string $path The path
@@ -37,14 +37,14 @@ implements IInjectable
 	 */
 	static public function pathToCamel($path) {
 		if (!is_string($path)) throw new \ErrorException();
-		
+
 		$segs = array_map(function($item) {
 			return ucfirst($item);
 		}, explode('/', $path));
-		
+
 		return lcfirst(implode('', $segs));
 	}
-	
+
 	/**
 	 * Returns a snake_case representation of <code>$path</code>
 	 * @param string $path The path
@@ -53,12 +53,53 @@ implements IInjectable
 	 */
 	static public function pathToSnake($path) {
 		if (!is_string($path)) throw new \ErrorException();
-		
+
 		return strtolower(str_replace('/', '_', $path));
 	}
-	
-	
-	
+
+
+
+	/**
+	 * Returns the method name of $action
+	 * @param string $action
+	 * @return string
+	 */
+	private function _getMethodName($action) {
+		return lcfirst($action) . 'Action';
+	}
+
+
+	/**
+	 * Returns true if $action exists, false otherwise
+	 * @param type $action
+	 * @return bool
+	 */
+	public function hasAction($action) {
+		return method_exists($this, $this->_getMethodName($action));
+	}
+
+	/**
+	 * Returns true if the action specified by $route exists, false otherwise
+	 * @param Route $route
+	 * @return bool
+	 */
+	public function isEnterable(Route $route) {
+		return $this->hasAction($route->getAction());
+	}
+
+	/**
+	 * Enters the action of the instance referenced by $route
+	 * @param Route $route The associated route
+	 * @return mixed
+	 */
+	public function enter(Route& $route) {
+		$method = $this->_getMethodName($route->getAction());
+
+		if (!method_exists($this, $method)) $method = $this->_getMethodName('default');
+
+		return $this->$method($route);
+	}
+
 	/**
 	 * Reenters the instance through $action
 	 * @param string $action The controller action
@@ -69,31 +110,17 @@ implements IInjectable
 	 */
 	protected function _reenter($action, Route& $route) {
 		if (!is_string($action) || empty($action)) throw new \ErrorException();
-		
-		$method = $action . 'Action';
-		
+
+		$method = $this->_getMethodName($action);
+
 		if (!method_exists($this, $method)) throw new \ErrorException();
-		
+
 		$route->setAction($action);
-		
+
 		return $this->$method($route);
 	}
-	
-	
-	/**
-	 * Enters the action of the instance referenced by $route
-	 * @param Route $route The associated route
-	 * @return mixed
-	 */
-	public function enter(Route& $route) {		
-		$method = $route->getAction() . 'Action';
-		
-		if (!method_exists($this, $method)) $method = 'defaultAction';
-		
-		return $this->$method($route);
-	}
-	
-	
+
+
 	/**
 	 * The default action of the controller
 	 * @param Route $route The associated route
