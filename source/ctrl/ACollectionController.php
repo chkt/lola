a5b74100ff51de2f8670d40fa4a087ca9a5455d7
chkt <ck@christoph-kettelhoit.de>
2016-11-25 20:43:36 +0800

    Fix ACollectionController

--- source/ctrl/ACollectionController.php
+++ source/ctrl/ACollectionController.php
@@ -4,85 +4,59 @@ namespace lola\ctrl;
 
 use lola\ctrl\AReplyController;
 
+use lola\io\http\HttpConfig;
 use lola\route\Route;
-
-use lola\http\HttpRequest;
-use lola\http\HttpReply;
-
+use lola\ctrl\RESTCollectionRequestTransform;
+use lola\ctrl\RESTReplyTransform;
 
 
-abstract class ACollectionController extends AReplyController {
-	
-	const VERSION = '0.1.0';
-		
-	
-	
+
+abstract class ACollectionController
+extends AReplyController
+{
+
+	const VERSION = '0.5.0';
+
+
+
 	protected $_itemController = '';
-	
-	
+
+
 	public function __construct() {
 		$this
-			->useRequestProcessor()
-			->append('resolve', function(AReplyController& $ctrl) {
-				$route =& $ctrl->useRoute();
-				$request =& $ctrl->useRequest();
-
-				$mime = $request->getPreferedAcceptMime([
-					HttpRequest::MIME_JSON
-				]);
-
-				if (empty($mime)) return 'unavailable';
-				
-				switch($request->getMethod()) {
-					case HttpRequest::METHOD_GET : return $route->setAction('read');
-					case HttpRequest::METHOD_PUT : return $route->setAction('create');
-					default : return $route->setAction('unavailable');
-				}
-			});
-			
-		$this
-			->useReplyProcessor()
-			->append('view', function(AReplyController& $ctrl) {
-				$json = $ctrl->useRoute()->useActionResult()->popItem();
-				
-				$ctrl
-					->useReply()
-					->setContent(json_encode($json))
-					->setMime(HttpReply::MIME_JSON);
-			});
+			->setRequestTransform(new RESTCollectionRequestTransform())
+			->setReplyProcessor(new RESTReplyTransform());
 	}
-	
-	
+
+
 	protected function createAction(Route $route) {
 		if (empty($this->_itemController)) return $this->unavailableAction($route);
-		
-		$request =& $this->useRequest();
-		
+
 		return $route
 			->setCtrl($this->_itemController)
 			->setAction('resolve')
-			->enter(function(AItemController $ctrl) use ($request) {
-				$ctrl->setRequest($request);
+			->enter(function(AItemController& $ctrl) {
+				$ctrl->setDriver($this->useDriver());
 			});
 	}
-	
-	
+
+
 	abstract protected function readAction(Route $route);
-	
-	
+
+
 	public function unavailableAction(Route $route) {
 		$this
 			->useReply()
-			->setCode(400)
-			->setMime(HttpReply::MIME_PLAIN)
+			->setCode(HttpConfig::CODE_NOT_VALID)
+			->setMime(HttpConfig::MIME_PLAIN)
 			->send();
 	}
-	
+
 	public function unauthenticatedAction(Route $route) {
 		$this
 			->useReply()
-			->setCode(403)
-			->setMime(HttpReply::MIME_PLAIN)
+			->setCode(HttpConfig::CODE_NOT_AUTH)
+			->setMime(HttpConfig::MIME_PLAIN)
 			->send();
 	}
 }
