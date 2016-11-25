0f666d064ce1058819f21735d156e47c3df0ffd2
chkt <ck@christoph-kettelhoit.de>
2016-11-25 20:43:10 +0800

    Add ACollectionControllerTest

---
+++ test/ctrl/ACollectionControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use lola\ctrl\ACollectionController;
+
+
+
+final class ACollectionControllerTest
+extends TestCase
+{
+
+	private function _getController($code, $mime) {
+		$reply = $this
+			->getMockBuilder('\lola\io\http\HttpReply')
+			->disableOriginalConstructor()
+			->setMethods([ 'setCode', 'setMime', 'send' ])
+			->getMock();
+
+		$reply
+			->expects($this->at(0))
+			->method('setCode')
+			->with($this->equalTo($code))
+			->willReturn($reply);
+
+		$reply
+			->expects($this->at(1))
+			->method('setMime')
+			->with($this->equalTo($mime))
+			->willReturn($reply);
+
+		$reply
+			->expects($this->at(2))
+			->method('send')
+			->willReturn($reply);
+
+		$ctrl = $this
+			->getMockBuilder('\lola\ctrl\ACollectionController')
+			->setMethods([ 'useReply' ])
+			->getMockForAbstractClass();
+
+		$ctrl
+			->expects($this->once())
+			->method('useReply')
+			->willReturn($reply);
+
+		return $ctrl;
+	}
+
+	private function _getRoute() {
+		return $this
+			->getMockBuilder('\lola\route\Route')
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+
+	public function test__construct() {
+		$ctrl = $this->getMockForAbstractClass('\lola\ctrl\ACollectionController');
+
+		$this->assertInstanceOf('\lola\ctrl\RESTCollectionRequestTransform', $ctrl->useRequestTransform());
+		$this->assertInstanceOf('\lola\ctrl\RESTReplyTransform', $ctrl->useReplyTransform());
+	}
+
+	public function testCreateAction() {
+		$route = $this
+			->getMockBuilder('\lola\route\Route')
+			->disableOriginalConstructor()
+			->setMethods([ 'setCtrl', 'setAction', 'enter' ])
+			->getMock();
+
+		$route
+			->expects($this->at(0))
+			->method('setCtrl')
+			->with($this->equalTo('foo'))
+			->willReturn($route);
+
+		$route
+			->expects($this->at(1))
+			->method('setAction')
+			->with($this->equalTo('resolve'))
+			->willReturn($route);
+
+		$route
+			->expects($this->at(2))
+			->method('enter')
+			->willReturn($route);
+
+		$ctrl = $this
+			->getMockBuilder('\lola\ctrl\ACollectionController')
+			->setMethods([ 'unavailableAction' ])
+			->getMockForAbstractClass();
+
+		$ctrl
+			->expects($this->at(0))
+			->method('unavailableAction')
+			->willReturn($ctrl);
+
+		$method = new \ReflectionMethod($ctrl, 'createAction');
+		$method->setAccessible(true);
+
+		$method->invoke($ctrl, $route);
+
+		$prop = new \ReflectionProperty($ctrl, '_itemController');
+		$prop->setAccessible(true);
+		$prop->setValue($ctrl, 'foo');
+
+		$method->invoke($ctrl, $route);
+	}
+
+	public function testUnavailableAction() {
+		$this
+			->_getController('400', 'text/plain')
+			->unavailableAction($this->_getRoute());
+	}
+
+	public function testUnauthenticatedAction() {
+		$this
+			->_getController('403', 'text/plain')
+			->unauthenticatedAction($this->_getRoute());
+	}
+}
