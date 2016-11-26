febdd0060598c72b3eb2968aa9950a8608011b09
chkt <ck@christoph-kettelhoit.de>
2016-11-26 11:21:52 +0800

    Update Form to use \lola\io\http\HttpRequest & HttpPayload

--- source/input/Form.php
+++ source/input/Form.php
@@ -5,66 +5,66 @@ namespace lola\input;
 use lola\inject\IInjectable;
 
 use lola\input\Processor;
-
-use lola\http\HttpRequest;
+use lola\io\http\HttpRequest;
 
 
 
 class Form
 implements IInjectable
 {
-	
+
 	const VERSION = '0.1.2';
-	
-	
-	
+
+
+
 	static public function getDependencyConfig(Array $config) {
 		return [];
 	}
-	
-	
-	
+
+
+
 	protected $_id = '';
 	protected $_processor = null;
-	
-	
+
+
 	public function __construct($id, Array $fields, Callable $cb = null) {
 		if (!is_string($id) || empty($id)) throw new ErrorException();
-		
+
 		$processor = Processor::Fields($fields);
 		$processor->setValidationCallback($cb);
-		
+
 		$this->_id = $id;
 		$this->_processor = $processor;
 	}
-	
-	
+
+
 	public function validate(HttpRequest $request) {
-		if (!$request->hasPayload()) return false;
-		
-		$data = $request->hasValidPayload() ? $request->getPayload() : [];
-		
+		if (empty($request->getBody())) return false;
+
+		$payload =& $request->usePayload();
+		$data = $payload->isValid() ? $payload->get() : [];
+
 		$this->_processor->validate($data);
-		
+
 		return $this->_processor->getState() === Processor::STATE_VALID;
 	}
-	
+
 	public function invalidate() {
 		$processor =& $this->_processor;
-				
+
 		$processor->setState($processor->getState() & ~Processor::FLAG_VALID);
-		
+
 		return $this;
 	}
-	
-	
+
+
 	public function getData() {
 		$res = $this->_processor->getData();
-		
+
 		$res['id'] = $this->_id;
-		
+
 		foreach ($res['field'] as & $field) $field['qname'] = $this->_id . '.' . $field['name'];
-			
+
 		return $res;
 	}
 }
