1896c4f1a8442411eef4247667b4afbea343512b
chkt <ck@christoph-kettelhoit.de>
2016-12-06 18:52:22 +0800

    Add ProviderProviderTest

---
+++ test/prov/ProviderProviderTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace test\prov;
+
+use PHPUnit\Framework\TestCase;
+
+use lola\app\IApp;
+use lola\inject\Injector;
+use lola\prov\AProvider;
+use lola\prov\ProviderProvider;
+
+
+
+final class ProviderProviderTest
+extends TestCase
+{
+
+	public function testLocate() {
+		$mockProv = $this
+			->getMockBuilder(AProvider::class)
+			->setConstructorArgs([function($location) {
+				return $location;
+			}])
+			->getMockForAbstractClass();
+
+		$injector = $this
+			->getMockBuilder(Injector::class)
+			->disableOriginalConstructor()
+			->setMethods([ 'produce' ])
+			->getMock();
+
+		$injector
+			->expects($this->exactly(4))
+			->method('produce')
+			->with($this->isType('string'))
+			->willReturnCallback(function($type) use ($mockProv) {
+				return $mockProv;
+			});
+
+		$app = $this
+			->getMockBuilder(IApp::class)
+			->setMethodsExcept()
+			->getMock();
+
+		$app
+			->expects($this->any())
+			->method('useInjector')
+			->willReturn($injector);
+
+		$app
+			->expects($this->any())
+			->method('hasProperty')
+			->willReturn(false);
+
+		$locator = new ProviderProvider($app);
+
+		$this->assertEquals('\\foo\\Bar', $locator->locate('class', '\\foo\\Bar'));
+		$this->assertEquals('//module/controller?id', $locator->locate('ctrl', '//module/controller?id'));
+		$this->assertEquals('//module/service?id', $locator->locate('service', '//module/service?id'));
+		$this->assertEquals('name', $locator->locate('environment', 'name'));
+	}
+}
