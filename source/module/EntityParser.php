b5a496dda5d351aa0f08052dbd237c9e776e7a58
chkt <ck@christoph-kettelhoit.de>
2016-12-07 14:47:40 +0800

    Add PROP_* constants, ->extractType()

--- source/module/EntityParser.php
+++ source/module/EntityParser.php
@@ -6,29 +6,57 @@ namespace lola\module;
 
 final class EntityParser
 {
-	
-	const VERSION = '0.3.2';
-	
-	
-	
+
+	const VERSION = '0.5.2';
+
+	const PROP_TYPE = 'type';
+	const PROP_LOCATION = 'location';
+	const PROP_MODULE = 'module';
+	const PROP_NAME = 'name';
+	const PROP_ID = 'id';
+
+
 	/**
 	 * Returns the entity definition referenced by hash
 	 * @param string $hash
 	 * @return array
-	 * @throws \ErrorException if $hash is not a nonempty string
+	 * @throws \ErrorException if $hash is empty
 	 */
-	static public function parse($hash) {
-		if (!is_string($hash) || empty($hash)) throw new \ErrorException();
-		
+	static public function parse(string $hash) {
+		if (empty($hash)) throw new \ErrorException();
+
 		$segs = parse_url($hash);
-		
+
 		if ($segs === false) throw new \ErrorException('MOD: hash malformed - ' . $hash);
 
 		return [
-			'type' => array_key_exists('scheme', $segs) ? $segs['scheme'] : '',
-			'module' => array_key_exists('host', $segs) ? $segs['host'] : '',
-			'name' => array_key_exists('path', $segs) ? str_replace('/', '\\', trim($segs['path'], '/')) : '',
-			'id' => array_key_exists('query', $segs) ? $segs['query'] : ''
+			self::PROP_TYPE => array_key_exists('scheme', $segs) ? $segs['scheme'] : '',
+			self::PROP_MODULE => array_key_exists('host', $segs) ? $segs['host'] : '',
+			self::PROP_NAME => array_key_exists('path', $segs) ? str_replace('/', '\\', trim($segs['path'], '/')) : '',
+			self::PROP_ID => array_key_exists('query', $segs) ? $segs['query'] : ''
+		];
+	}
+
+	/**
+	 * Returns the entity type and compound identifier referenced by hash
+	 * @param string $hash
+	 * @return array
+	 * @throws \ErrorException if $hash is empty
+	 */
+	static public function extractType(string $hash) {
+		if (empty($hash)) throw new \ErrorException();
+
+		$type = parse_url($hash, PHP_URL_SCHEME);
+
+		if (!is_null($type)) $location = substr($hash, strlen($type) + 1);
+		else {
+			$type = '';
+			$location = $hash;
+		}
+
+		return [
+			self::PROP_TYPE => $type,
+			self::PROP_LOCATION => $location
 		];
 	}
 }
