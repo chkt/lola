3147e0c8d947a7dfe2f7d20d7c5aeee48dd94378
chkt <ck@christoph-kettelhoit.de>
2016-12-14 21:51:03 +0800

    Change Registry to be injectable

--- source/module/Registry.php
+++ source/module/Registry.php
@@ -2,6 +2,7 @@
 
 namespace lola\module;
 
+use lola\inject\IInjectable;
 use lola\app\IApp;
 
 use lola\module\EntityParser;
@@ -9,15 +10,22 @@ use lola\module\IModule;
 
 
 
-class Registry {
+class Registry
+implements IInjectable
+{
 
-	const VERSION = '0.3.2';
+	const VERSION = '0.5.2';
 
-	
-	
+
+	static public function getDependencyConfig(array $config) {
+		return [ 'resolve:app' ];
+	}
+
+
+
 	private $_app = null;
 	private $_injector = null;
-	
+
 	private $_defered = null;
 	private $_modules = null;
 
@@ -28,7 +36,7 @@ class Registry {
 	public function __construct(IApp& $app) {
 		$this->_app =& $app;
 		$this->_injector = null;
-		
+
 		$this->_defered = [];
 		$this->_modules = [];
 	}
@@ -43,8 +51,8 @@ class Registry {
 
 		return $this->_injector;
 	}
-	
-	
+
+
 	/**
 	 * Returns the classpath referenced by $moduleName, $entityType and $entityName
 	 * @param string $moduleName
@@ -54,15 +62,15 @@ class Registry {
 	 */
 	private function _getClassPath($moduleName, $entityType, $entityName) {
 		if (!array_key_exists($moduleName, $this->_modules)) $this->_loadModule($moduleName);
-		
+
 		$module = $this->_modules[$moduleName];
 		$locatorConfig = array_key_exists('locator', $module) ? $module['locator'] : [];
 		$config = array_key_exists($entityType, $locatorConfig) ? $locatorConfig[$entityType] : [];
-		
+
 		$path = array_key_exists('path', $config) ? $config['path'] : $entityType;
 		$pre = array_key_exists('prefix', $config) ? $config['prefix'] : '';
 		$post = array_key_exists('postfix', $config) ? $config['postfix'] : ucfirst($entityType);
-		
+
 		return '\\' . $moduleName . '\\' . $path . '\\' . ucfirst($pre . $entityName . $post);
 	}
 
@@ -77,13 +85,13 @@ class Registry {
 	 */
 	private function _locateEntity($entityType, $entityName, $entityId) {
 		if (!empty($this->_defered)) $this->_loadDefered();
-		
+
 		foreach ($this->_modules as $moduleName => $module) {
 			$qname = $this->_getClassPath($moduleName, $entityType, $entityName);
-			
+
 			if (class_exists($qname)) return $this->_useInjector()->produce($qname, [ 'id' => $entityId ]);
 		}
-		
+
 		throw new \ErrorException('MOD: entity missing: ' . $entityType . '|' . $entityName);
 	}
 
@@ -98,31 +106,31 @@ class Registry {
 	 */
 	private function _produceEntity($moduleName, $entityType, $entityName, $entityId) {
 		$qname = $this->_getClassPath($moduleName, $entityType, $entityName);
-		
+
 		if (class_exists($qname)) return $this->_useInjector()->produce($qname, [ 'id' => $entityId ]);
-		
+
 		throw new \ErrorException('MOD: entity missing: ' . $entityType . '|' . $entityName);
 	}
-	
-	
+
+
 	/**
 	 * Applies the entity config specified by $config
 	 * @param array $config The entity config
 	 */
 	private function _applyConfig(array $config) {
 		$locator = $this->_app->useLocator();
-		
+
 		foreach ($config as $hash => $fn) {
 			$entity = EntityParser::parse($hash);
 			$id = $entity['name'] . (!empty($entity['id']) ? '?' . $entity['id'] : '');
-			
+
 			$locator
 				->using($entity['type'])
 				->configure($id, $fn);
 		}
 	}
-	
-	
+
+
 	/**
 	 * Loads the module referenced by $name
 	 * @param string $name The module name
@@ -136,28 +144,28 @@ class Registry {
 		if (!($loader instanceof IModule)) throw new \ErrorException('MOD: no loader');
 
 		$module = $loader->getModuleConfig();
-		
+
 		if (array_key_exists('config', $module)) {
 			$this->_applyConfig($module['config']);
-			
+
 			unset($module['config']);
 		}
-		
+
 		$this->_modules[$name] = $module;
 	}
-	
+
 	/**
 	 * Loads all defered modules
 	 */
 	private function _loadDefered() {
 		$defered = $this->_defered;
 		$this->_defered = [];
-		
+
 		foreach ($defered as $name) {
 			if (!array_key_exists($name, $this->_modules)) $this->_loadModule($name);
 		}
 	}
-	
+
 
 	/**
 	 * Adds the module referenced by $name to the defered modules
@@ -167,9 +175,9 @@ class Registry {
 	 */
 	public function loadModule($name) {
 		if (!is_string($name) || empty($name)) throw new \ErrorException();
-		
+
 		$this->_defered[] = $name;
-		
+
 		return $this;
 	}
 
@@ -185,12 +193,12 @@ class Registry {
 
 		if (array_key_exists('config', $module)) {
 			$this->_applyConfig($module['config']);
-			
+
 			unset($module['config']);
 		}
-		
+
 		$this->_modules[$name] = $module;
-		
+
 		return $this;
 	}
 
@@ -203,11 +211,11 @@ class Registry {
 	 */
 	public function resolve($type, $hash) {
 		$segs = EntityParser::parse($hash);
-		
+
 		return $this->produce($type, $segs['name'], $segs['id'], $segs['module']);
 	}
-	
-	
+
+
 	/**
 	 * Returns the entity referenced by $type, $name, $id and $module
 	 * @param string $type The entity type
@@ -226,7 +234,7 @@ class Registry {
 			!is_string($name) || empty($name) ||
 			!is_string($id) || !is_string($module)
 		) throw new \ErrorException();
-		
+
 		if (empty($module)) return $this->_locateEntity($type, $name, $id);
 		else return $this->_produceEntity($module, $type, $name, $id);
 	}
