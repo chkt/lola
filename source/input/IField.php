9bb639a18c6560f9a717656b6b388f9513b7f874
chkt <ck@christoph-kettelhoit.de>
2017-02-06 13:27:14 +0800

    Update IField

--- source/input/IField.php
+++ source/input/IField.php
@@ -2,48 +2,40 @@
 
 namespace lola\input;
 
+use lola\type\IProjectable;
 
 
-interface IField {
+
+interface IField
+extends IProjectable
+{
+
+	public function isChanged() : bool;
+
+	public function isEmpty() : bool;
+
+	public function isMultiValue() : bool;
+
+	public function isImmutable() : bool;
+
+	public function isSubmit() : bool;
+
 	
-	public function isInitial();
-	
-	public function isChanged();
-	
-	
-	public function isEmpty();
-	
-	public function isNonEmpty();
-	
-	
-	public function isMutable();
-	
-	public function isValidating();
-	
-	public function isValid();
-	
-	public function isSubmit();
-	
-	public function isMultiple();
-	
-	
-	public function getName();
-	
-	
-	public function getValue();
-	
-	public function setValue($value);
-	
-	
-	public function getValues();
-	
-	public function setValues(Array $values);
-	
-	public function mapValues(Array $values);
-	
-	
-	public function getData();
-	
-	
-	public function invalidate($state);
+	public function isValid() : bool;
+
+
+	public function getName() : string;
+
+
+	public function getValue() : string;
+
+	public function setValue(string $value) : IField;
+
+
+	public function getValues() : array;
+
+	public function setValues(array $values) : IField;
+
+
+	public function invalidate(int $state) : IField;
 }
