bf748ea0edaa0d1e9843bf8ca5382ac85c39b6a2
chkt <ck@christoph-kettelhoit.de>
2017-02-12 12:50:06 +0800

    Remove ->isFinal()

--- source/input/valid/ValidationException.php
+++ source/input/valid/ValidationException.php
@@ -16,26 +16,15 @@ implements IValidationException
 
 
 
-	private $_final;
-
-
-	public function __construct(string $message, int $code = 0, bool $final = false) {
+	public function __construct(string $message, int $code = 0) {
 		parent::__construct($message, $code);
-
-		$this->_final = $final;
-	}
-
-
-	public function isFinal() : bool {
-		return $this->_final;
 	}
 
 
 	public function getProjection(array $selection = []) : array {
 		return [
 			'message' => $this->getMessage(),
-			'code' => $this->getCode(),
-			'final' => $this->isFinal()
+			'code' => $this->getCode()
 		];
 	}
 }
