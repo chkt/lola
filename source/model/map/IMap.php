59285ab0e59fd35037744539f9fd9ec895b4fef6
chkt <ck@christoph-kettelhoit.de>
2017-02-20 12:51:48 +0700

    Add IMap

---
+++ source/model/map/IMap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace lola\model\map;
+
+
+
+interface IMap
+{
+
+	public function hasKey(string $key) : bool;
+
+
+	public function getBool(string $key) : bool;
+
+	public function setBool(string $key, bool $value) : IMap;
+
+
+	public function getInt(string $key) : int;
+
+	public function setInt(string $key, int $value) : IMap;
+
+
+	public function getFloat(string $key) : float;
+
+	public function setFloat(string $key, float $value) : IMap;
+
+
+	public function getString(string $key): string;
+
+	public function setString(string $key, string $value) : IMap;
+}
