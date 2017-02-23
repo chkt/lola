9990b678a5f939ed5f8df36fff072e6117531459
chkt <ck@christoph-kettelhoit.de>
2017-02-23 12:57:02 +0700

    Add FloatTransform

---
+++ source/input/valid/step/FloatTransform.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lola\input\valid\step;
+
+use lola\input\valid\AValidationTransform;
+
+use lola\input\valid\ValidationException;
+
+
+
+final class FloatTransform
+extends AValidationTransform
+{
+
+
+	public function getId() : string {
+		return 'float';
+	}
+
+
+	protected function _validate($source) {
+		if (is_array($source) || is_object($source)) throw new ValidationException($this->getId() . 'nonScalar', 1);
+
+		return (float) $source;
+	}
+
+	protected function _transform($source, $result) {
+		return $result;
+	}
+}
