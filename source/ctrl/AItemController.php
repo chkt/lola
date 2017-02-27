2b415794c73d0c29bc3269b60368b2e42d3b1552
chkt <ck@christoph-kettelhoit.de>
2017-02-27 15:07:22 +0700

    Add ->invalidAction()

--- source/ctrl/AItemController.php
+++ source/ctrl/AItemController.php
@@ -16,10 +16,6 @@ abstract class AItemController
 extends AReplyController
 {
 
-	const VERSION = '0.5.0';
-
-
-
 	public function __construct(IHttpDriver& $driver) {
 		parent::__construct($driver);
 
@@ -37,6 +33,14 @@ extends AReplyController
 
 	abstract protected function deleteAction(Route $route);
 
+
+	public function invalidAction(Route $route) {
+		$this
+			->useReply()
+			->setCode(IHttpConfig::CODE_NOT_VALID)
+			->setMime(IHttpConfig::MIME_PLAIN)
+			->send();
+	}
 
 	public function unavailableAction(Route $route) {
 		$this
