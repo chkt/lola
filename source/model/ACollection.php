43170798d26b5fe64a14ae7746738c5b0530ef70
chkt <ck@christoph-kettelhoit.de>
2017-02-27 21:06:06 +0700

    Update to implement ICollection

--- source/model/ACollection.php
+++ source/model/ACollection.php
@@ -3,65 +3,65 @@
 namespace lola\model;
 
 use lola\type\AIterateable;
+use lola\model\collection\ICollection;
 
+use lola\model\IModel;
+use lola\model\IResourceQuery;
 use lola\model\IResourceCollection;
 
 
 
 abstract class ACollection
 extends AIterateable
+implements ICollection
 {
-	
-	const VERSION = '0.2.4';
-	
-	
-	
+
 	private $_resource = null;
 	private $_factory = null;
-		
-	
+
+
 	public function __construct(IResourceCollection $resource, Callable $itemFactory) {
 		parent::__construct();
-		
+
 		$this->_resource = $resource;
 		$this->_factory = $itemFactory;
-		
+
 		$this->_length = $resource->getLength();
 	}
-	
-	
+
+
 	protected function& _useItem($index) {
 		$items =& $this->_items;
-		
+
 		if (!array_key_exists($index, $items)) {
 			$resource =& $this->_resource->useItem($index);
 			$items[$index] = call_user_func_array($this->_factory, [ & $resource ]);
 		}
-		
+
 		return $items[$index];
 	}
-	
-	
-	public function isLive() {
+
+
+	public function isLive() : bool {
 		return $this->_resource->isLive();
 	}
-	
-	
-	public function hasItem(IResourceQuery $query) {
+
+
+	public function hasItem(IResourceQuery $query) : bool {
 		return $this->_resource->getIndexOf($query) !== -1;
 	}
-	
-	public function& useItem(IResourceQuery $query) {
+
+	public function& useItem(IResourceQuery $query) : IModel {
 		$index = $this->_resource->getIndexOf($query);
 		$null = null;
-		
+
 		return $index !== -1 ? $this->_useItem($index) : $null;
 	}
-	
-	
-	public function update() {
+
+
+	public function update() : ICollection {
 		$this->_resource->update();
-		
+
 		return $this;
 	}
 }
