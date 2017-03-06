e44a33f08d379b75971b59f392268a2bfa70338f
chkt <ck@christoph-kettelhoit.de>
2017-03-06 20:21:19 +0700

    Add List, Map & Set accessors

--- source/model/map/IResourceMap.php
+++ source/model/map/IResourceMap.php
@@ -30,6 +30,21 @@ interface IResourceMap
 	public function setString(string $key, string $value) : IResourceMap;
 
 
+	public function getList(string $key) : array;
+
+	public function setList(string $key, array $list) : IResourceMap;
+
+
+	public function getSet(string $key) : array;
+
+	public function setSet(string $key, array $set) : IResourceMap;
+
+
+	public function getMap(string $key) : array;
+
+	public function setMap(string $key, array $map) : IResourceMap;
+
+
 	public function removeKey(string $key) : IResourceMap;
 
 	public function renameKey(string $key, string $to) : IResourceMap;
