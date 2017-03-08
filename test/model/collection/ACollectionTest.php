75dfa192dc6f235d3a69ff1905a6c4859730c5f5
chkt <ck@christoph-kettelhoit.de>
2017-03-08 11:14:56 +0700

    Refactor to drop MockModel; Add ->testGetProjection()

--- test/model/collection/ACollectionTest.php
+++ test/model/collection/ACollectionTest.php
@@ -4,8 +4,8 @@ namespace test\model\collection;
 
 use PHPUnit\Framework\TestCase;
 
-use test\model\collection\MockModel;
 use lola\inject\IInjector;
+use lola\model\IModel;
 use lola\model\IResource;
 use lola\model\AResourceDependencyFactory;
 use lola\model\collection\ACollection;
@@ -35,6 +35,12 @@ extends TestCase
 			->getMock();
 	}
 
+	private function _mockModel() {
+		return $this
+			->getMockBuilder(IModel::class)
+			->getMock();
+	}
+
 	private function _mockCollection(
 		IInjector& $injector,
 		IResourceCollection& $collection,
@@ -57,14 +63,15 @@ extends TestCase
 		$injector
 			->expects($this->exactly(1))
 			->method('produce')
-			->with($this->equalTo(MockModel::class), $this->isType('array'))
-			->willReturnCallback(function($name, array $config) {
+			->with($this->equalTo('foo'), $this->isType('array'))
+			->willReturnCallback(function(string $name, array $config) : IModel {
+				$this->assertEquals('foo', $name);
 				$this->assertArrayHasKey('mode', $config);
 				$this->assertEquals(AResourceDependencyFactory::MODE_PASS, $config['mode']);
 				$this->assertArrayHasKey('resource', $config);
 				$this->assertInstanceOf(IResource::class, $config['resource']);
 
-				return new $name();
+				return $this->_mockModel();
 			});
 
 		$resource = $this->_mockResourceCollection();
@@ -81,15 +88,62 @@ extends TestCase
 			->with()
 			->willReturn(1);
 
-		$collection = $this->_mockCollection($injector, $resource, MockModel::class);
+		$collection = $this->_mockCollection($injector, $resource, 'foo');
 
 		$item0 =& $collection->useIndex(0);
 
-		$this->assertInstanceOf(MockModel::class, $item0);
+		$this->assertInstanceOf(IModel::class, $item0);
 
 		$item1 =& $collection->useIndex(0);
 
-		$this->assertInstanceOf(MockModel::class, $item1);
+		$this->assertInstanceOf(IModel::class, $item1);
 		$this->assertSame($item1, $item0);
+	}
+
+
+	public function testGetProjection() {
+		$injector = $this->_mockInjector();
+
+		$injector
+			->expects($this->once())
+			->method('produce')
+			->with($this->equalTo('foo'), $this->isType('array'))
+			->willReturnCallback(function() : IModel {
+				$model = $this->_mockModel();
+
+				$model
+					->expects($this->once())
+					->method('getProjection')
+					->with($this->isType('array'))
+					->willReturn([
+						'foo' => 1,
+						'bar' => 2
+					]);
+
+				return $model;
+			});
+
+		$resource = $this->_mockResourceCollection();
+
+		$resource
+			->expects($this->at(0))
+			->method('getLength')
+			->with()
+			->willReturn(1);
+
+		$resource
+			->expects($this->at(1))
+			->method('useItem')
+			->with()
+			->willReturnCallback(function() {
+				return $this->_mockResource();
+			});
+
+		$collection = $this->_mockCollection($injector, $resource, 'foo');
+
+		$this->assertEquals([[
+			'foo' => 1,
+			'bar' => 2
+		]], $collection->getProjection());
 	}
 }
