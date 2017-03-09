5297a69a8edbb1d5c905ae132f96233c3fc63303
chkt <ck@christoph-kettelhoit.de>
2017-03-09 15:22:28 +0700

    Change ModelActionLog to depend on ASizedIterateable

--- source/model/ModelActionLog.php
+++ source/model/ModelActionLog.php
@@ -2,46 +2,64 @@
 
 namespace lola\model;
 
-use lola\type\AIterateable;
+use lola\type\ASizedIterateable;
 
 
 
 final class ModelActionLog
-extends AIterateable
+extends ASizedIterateable
 {
-	
-	const VERSION = '0.2.1';
-	
-	
-	
+
+	private $_items;
+	private $_length;
+
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->_items = [];
+		$this->_length = 0;
+	}
+
+
+	public function getLength() : int {
+		return $this->_length;
+	}
+
+
+	protected function& _useItem(int $index) {
+		return $this->_items[$index];
+	}
+
+
 	public function push($prop, $old, $new) {
 		if (!is_string($prop) || empty($prop)) throw new \ErrorException();
-		
+
 		$this->_items[] = [
 			'property' => $prop,
 			'oldData' => $old,
 			'newData' => $new
 		];
-		
+
 		$this->_length += 1;
-		
+
 		return $this;
 	}
-	
+
 	public function pop() {
 		$res = array_pop($this->_items);
-		
+
 		$this->_length -= 1;
-		
+
 		return $res;
 	}
-	
+
 	public function clear() {
 		$this->_items = [];
 
-		$this->_cursor = 0;		
+		$this->_cursor = 0;
 		$this->_length = 0;
-		
+
 		return $this;
 	}
 }
