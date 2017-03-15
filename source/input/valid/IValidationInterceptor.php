cb1ead1979865a8ae85d77a5b195fba2273732d3
chkt <ck@christoph-kettelhoit.de>
2017-03-15 22:51:32 +0700

    Change signature to require IValidationTransform

--- source/input/valid/IValidationInterceptor.php
+++ source/input/valid/IValidationInterceptor.php
@@ -2,10 +2,12 @@
 
 namespace lola\input\valid;
 
+use lola\input\valid\IValidationTransform;
+
 
 
 interface IValidationInterceptor
 {
 
-	public function intercept(IValidationStep& $step);
+	public function intercept(string $chain, IValidationTransform& $step);
 }
