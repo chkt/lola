f76236a8fecb67c277d23b430e1e91ca1e2d830b
chkt <ck@christoph-kettelhoit.de>
2017-03-15 21:33:08 +0700

    Remove AValidationStep dependency Add chain processing

--- source/input/valid/AValidationTransform.php
+++ source/input/valid/AValidationTransform.php
@@ -2,63 +2,121 @@
 
 namespace lola\input\valid;
 
-use lola\input\valid\IValidationStep;
-use lola\input\valid\AValidationStep;
 use lola\input\valid\IValidationTransform;
+
+use lola\input\valid\IValidationException;
 
 
 
 abstract class AValidationTransform
-extends AValidationStep
 implements IValidationTransform
 {
 
-	const VERSION = '0.6.0';
-
-
+	private $_validated;
+	private $_source;
+	private $_result;
+	private $_error;
 
 	private $_nextStep;
-	private $_transformed;
 
 
-	public function __construct(IValidationStep $next) {
-		parent::__construct();
+	public function __construct(IValidationTransform $next = null) {
+		$this->_validated = false;
+		$this->_source = null;
+		$this->_result = null;
+		$this->_error = null;
 
 		$this->_nextStep = $next;
-		$this->_transformed = null;
 	}
 
 
-	public function wasTransformed() : bool {
-		return !is_null($this->_transformed);
+	public function wasValidated() : bool {
+		return $this->_validated;
+	}
+
+	public function isValid() : bool {
+		return $this->_validated && is_null($this->_error);
 	}
 
 
-	public function getNextStep() : IValidationStep {
+	public function getSource() {
+		return $this->_source;
+	}
+
+	public function getResult() {
+		return $this->_result;
+	}
+
+	public function getError() : IValidationException {
+		if (!$this->_validated || is_null($this->_error)) throw new \ErrorException();
+
+		return $this->_error;
+	}
+
+
+	public function hasNextStep() : bool {
+		return !is_null($this->_nextStep);
+	}
+
+	public function& useNextStep() : IValidationTransform {
+		if (!$this->hasNextStep()) throw new \ErrorException();
+
 		return $this->_nextStep;
 	}
 
-	public function getTransformedResult() {
-		if (!$this->wasTransformed()) throw new \ErrorException();
 
-		return $this->_transformed;
+	abstract protected function _validate($source);
+
+	protected function _transform($source, $result) {
+		return $result;
 	}
 
 
-	public function reset() : IValidationStep {
-		$this->_nextStep->reset();
+	protected function _validateNextStep(IValidationTransform& $next, $source) {
+		$result = $this->getSource();
 
-		return parent::reset();
+		$next->validate($source);
+
+		if ($next->isValid()) $result = $this->_transform($this->getSource(), $next->getResult());
+		else $this->_error = $next->getError();
+
+		return $result;
 	}
 
 
-	abstract protected function _transform($source, $result);
+	public function validate($source) : IValidationTransform {
+		$this->_validated = false;
+		$this->_source = $source;
+		$this->_result = $source;
+		$this->_error = null;
 
+		try {
+			$result = $this->_validate($source);
+		}
+		catch (IValidationException $ex) {
+			$this->_validated = true;
+			$this->_error = $ex;
 
-	public function transform($result) : IValidationTransform {
-		if (!$this->wasValidated()) throw new \ErrorException();
+			return $this;
+		}
 
-		$this->_transformed = $this->_transform($this->getSource(), $result);
+		if ($this->hasNextStep()) $result = $this->_validateNextStep($this->useNextStep(), $result);
+
+		$this->_validated = true;
+		$this->_result = $result;
+
+		return $this;
+	}
+
+	public function reset() : IValidationTransform {
+		$this->_validated = false;
+		$this->_source = null;
+		$this->_result = null;
+		$this->_error = null;
+
+		if ($this->hasNextStep()) $this
+			->useNextStep()
+			->reset();
 
 		return $this;
 	}
