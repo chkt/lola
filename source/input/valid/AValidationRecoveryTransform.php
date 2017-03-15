6532a17b68bd70278e1dc0893c49d9bdeff6afee
chkt <ck@christoph-kettelhoit.de>
2017-03-15 22:30:04 +0700

    Rename and update class

---
+++ source/input/valid/AValidationRecoveryTransform.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace lola\input\valid;
+
+use lola\input\valid\AValidationTransform;
+use lola\input\valid\IValidationRecoveryTransform;
+
+
+
+abstract class AValidationRecoveryTransform
+extends AValidationTransform
+implements IValidationRecoveryTransform
+{
+
+	private $_recovered;
+
+
+	public function __construct(IValidationTransform $next) {
+		parent::__construct($next);
+
+		$this->_recovered = false;
+	}
+
+
+	public function wasRecovered() : bool {
+		return $this->_recovered;
+	}
+
+
+	abstract protected function _recover(IValidationException $exception);
+
+
+	protected function _validateNextStep(IValidationTransform &$next, $source) {
+		$result = $this->getSource();
+
+		$next->validate($source);
+
+		if ($next->isValid()) $result = $this->_transform($this->getSource(), $next->getResult());
+		else {
+			$result = $this->_recover($next->getError());
+			$this->_recovered = true;
+		}
+
+		return $result;
+	}
+
+	public function reset() : IValidationTransform {
+		$this->_recovered = false;
+
+		return parent::reset();
+	}
+}
