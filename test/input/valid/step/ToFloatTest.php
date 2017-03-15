8c794f49391d0725dd9bbc2c519efef7c53963e4
chkt <ck@christoph-kettelhoit.de>
2017-03-15 23:08:24 +0700

    Rename ToFloatTest; Update test cases

--- test/input/valid/step/ToFloatTest.php
+++ test/input/valid/step/ToFloatTest.php
@@ -4,19 +4,19 @@ namespace test\input\valid\step;
 
 use PHPUnit\Framework\TestCase;
 
-use lola\input\valid\step\NoopValidationStep;
-use lola\input\valid\step\FloatTransform;
+use lola\input\valid\step\Noop;
+use lola\input\valid\step\ToFloat;
 
 
 
-final class FloatTransformTest
+final class ToFloatTest
 extends TestCase
 {
 
 	private function _produceStep() {
-		$next = new NoopValidationStep();
+		$next = new Noop();
 
-		return new FloatTransform($next);
+		return new ToFloat($next);
 	}
 
 
@@ -33,30 +33,20 @@ extends TestCase
 		$this->assertTrue($step->wasValidated());
 		$this->assertTrue($step->isValid());
 		$this->assertInternalType('float', $step->getResult());
+		$this->assertEquals(0.0, $step->getResult());
 
 		$step->validate(0);
 		$this->assertInternalType('float', $step->getResult());
+		$this->assertEquals(0.0, $step->getResult());
 
 		$step->validate(false);
 		$this->assertInternalType('float', $step->getResult());
+		$this->assertEquals(0.0, $step->getResult());
 
 		$step->validate([]);
 		$this->assertFalse($step->isValid());
 
 		$step->validate(new \stdClass());
 		$this->assertFalse($step->isValid());
-	}
-
-	public function testTransform() {
-		$step = $this->_produceStep();
-
-		$step->validate(0.0);
-
-		$step->transform(0.0);
-		$this->assertEquals(0.0, $step->getTransformedResult());
-		$step->transform(1.1);
-		$this->assertEquals(1.1, $step->getTransformedResult(1.1));
-		$step->transform(-1.0e-8);
-		$this->assertEquals(-1.0e-8, $step->getTransformedResult());
 	}
 }
