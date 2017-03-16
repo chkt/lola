9413eff0fcdd0be4be6e838b667c5061c5f0c23b
chkt <ck@christoph-kettelhoit.de>
2017-03-16 07:57:31 +0700

    Rename ArrayPropSelect

--- source/input/valid/step/ArrayPropSelect.php
+++ source/input/valid/step/ArrayPropSelect.php
@@ -3,20 +3,20 @@
 namespace lola\input\valid\step;
 
 use lola\input\valid\AValidationTransform;
-use lola\input\valid\IValidationStep;
+use lola\input\valid\IValidationTransform;
 
 use lola\input\valid\ValidationException;
 
 
 
-final class ArrayPropTransform
+final class ArrayPropSelect
 extends AValidationTransform
 {
 
 	private $_prop;
 
 
-	public function __construct(string $prop, IValidationStep $next) {
+	public function __construct(string $prop, IValidationTransform $next) {
 		if (empty($prop)) throw new \ErrorException();
 
 		parent::__construct($next);
@@ -26,7 +26,7 @@ extends AValidationTransform
 
 
 	public function getId() : string {
-		return 'arrayProp.' . $this->_prop;
+		return 'arrayPropSelect.' . $this->_prop;
 	}
 
 
