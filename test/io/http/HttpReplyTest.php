c31dff2e86f028e249dbbf3c3f543428a31472ab
chkt <ck@chkt.one>
2017-07-03 01:44:28 +0200

    Update HttpReplyTest

--- test/io/http/HttpReplyTest.php
+++ test/io/http/HttpReplyTest.php
@@ -5,7 +5,14 @@ namespace test\io\http;
 use PHPUnit\Framework\TestCase;
 
 use lola\io\http\IHttpConfig;
+use lola\io\http\IHttpMessage;
+use lola\io\http\IHttpRequest;
+use lola\io\http\IHttpCookies;
+use lola\io\http\IHttpDriver;
+use lola\io\http\HttpMessage;
 use lola\io\http\HttpReply;
+use lola\io\http\HttpConfig;
+use lola\io\mime\IMimePayload;
 
 
 
@@ -13,128 +20,187 @@ class HttpReplyTest
 extends TestCase
 {
 
-	private $_driver;
-
-
-	public function __construct() {
-		parent::__construct();
-
-		$this->_driver = new MockDriver();
-		$this->_driver
-			->useConfig()
-			->addRule(IHttpConfig::RULE_ENCODING, 'iso-8859-1');
+	private function _produceConfig() : IHttpConfig {
+		return new HttpConfig();
 	}
 
+	private function _produceMessage(string $line = null, array $headers = null, string $body = null) : IHttpMessage {
+		if (is_null($line)) $line = 'HTTP/1.1 200 OK';
+		if (is_null($headers)) $headers = [
+			IHttpMessage::HEADER_CONTENT_TYPE => ['text/plain;charset=iso-8859-1']
+		];
+		if (is_null($body)) $body = '200 - OK';
 
-	private function _produceReply() {
-		return new HttpReply($this->_driver);
+		return new HttpMessage($line, $headers, $body);
+	}
+
+	private function _produceMock(string $qname) {
+		return $this
+			->getMockBuilder($qname)
+			->getMock();
+	}
+
+	private function _mockDriver(IHttpMessage $message = null) {
+		if (is_null($message)) $message = $this->_produceMessage();
+
+		$config = $this->_produceConfig();
+		$request = $this->_produceMock(IHttpRequest::class);
+		$cookies = $this->_produceMock(IHttpCookies::class);
+		$mime = $this->_produceMock(IMimePayload::class);
+
+		$driver = $this
+			->getMockBuilder(IHttpDriver::class)
+			->getMock();
+
+		$driver
+			->expects($this->any())
+			->method('useConfig')
+			->with()
+			->willReturnReference($config);
+
+		$driver
+			->expects($this->any())
+			->method('useReplyMessage')
+			->with()
+			->willReturnReference($message);
+
+		$driver
+			->expects($this->any())
+			->method('useRequest')
+			->with()
+			->willReturnReference($request);
+
+		$driver
+			->expects($this->any())
+			->method('useCookies')
+			->with()
+			->willReturnReference($cookies);
+
+		$driver
+			->expects($this->any())
+			->method('useReplyPayload')
+			->with()
+			->willReturnReference($mime);
+
+		$driver
+			->expects($this->any())
+			->method('sendReply')
+			->with()
+			->willReturn(true);
+
+		return $driver;
+	}
+
+	private function _produceReply(IHttpDriver $driver = null) {
+		if (is_null($driver)) $driver = $this->_mockDriver();
+
+		return new HttpReply($driver);
 	}
 
 
 	public function testUsePayload() {
-		$reply = $this->_produceReply();
+		$driver = $this->_mockDriver();
+		$reply = $this->_produceReply($driver);
 
-		$this->assertEquals($this->_driver->useReplyPayload(), $reply->usePayload());
+		$this->assertSame($driver->useReplyPayload(), $reply->usePayload());
 	}
 
 	public function testUseRequest() {
-		$reply = new HttpReply($this->_driver);
+		$driver = $this->_mockDriver();
+		$reply = $this->_produceReply($driver);
 
-		$this->assertEquals($this->_driver->useRequest(), $reply->useRequest());
+		$this->assertSame($driver->useRequest(), $reply->useRequest());
 	}
 
 	public function testUseCookies() {
-		$reply = new HttpReply($this->_driver);
+		$driver = $this->_mockDriver();
+		$reply = $this->_produceReply($driver);
 
-		$this->assertEquals($this->_driver->useCookies(), $reply->useCookies());
+		$this->assertSame($driver->useCookies(), $reply->useCookies());
 	}
 
 
 	public function testGetCode() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getCode(), '200');
+		$this->assertEquals('200', $reply->getCode());
 	}
 
 	public function testSetCode() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setCode('404'), $reply);
-		$this->assertEquals($reply->getCode(), '404');
+		$this->assertSame($reply, $reply->setCode('404'));
+		$this->assertEquals('404', $reply->getCode());
 	}
 
 	public function testGetCodeHeader() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$reply->setCode(IHttpConfig::CODE_NOT_FOUND);
-
+		$this->assertSame($reply, $reply->setCode(IHttpConfig::CODE_NOT_FOUND));
 		$this->assertEquals('HTTP/1.1 404 Not Found', $reply->getCodeHeader());
 	}
 
 	public function testGetCodeMessage() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$reply->setCode(IHttpConfig::CODE_NOT_FOUND);
-
+		$this->assertSame($reply, $reply->setCode(IHttpConfig::CODE_NOT_FOUND));
 		$this->assertEquals('404 - Page not found', $reply->getCodeMessage());
 	}
 
 
 	public function testGetMime() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getMime(), 'text/plain');
+		$this->assertEquals('text/plain', $reply->getMime());
 	}
 
 	public function testSetMime() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setMime('text/html'), $reply);
-		$this->assertEquals($reply->getMime(), 'text/html');
+		$this->assertSame($reply, $reply->setMime('text/html'));
+		$this->assertEquals('text/html', $reply->getMime());
 	}
 
 	public function testGetEncoding() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getEncoding(), 'utf-8');
+		$this->assertEquals('iso-8859-1', $reply->getEncoding());
 	}
 
 	public function testSetEncoding() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setEncoding('iso-8859-1'), $reply);
-		$this->assertEquals($reply->getEncoding(), 'iso-8859-1');
+		$this->assertSame($reply, $reply->setEncoding('utf-8'));
+		$this->assertEquals('utf-8', $reply->getEncoding());
 	}
 
 	public function testIsRedirect() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
 		$this->assertFalse($reply->isRedirect());
 
-		$reply->setCode('301');
-
+		$this->assertSame($reply, $reply->setCode('301'));
 		$this->assertTrue($reply->isRedirect());
 
-		$reply->setCode('404');
-
+		$this->assertSame($reply, $reply->setCode('404'));
 		$this->assertFalse($reply->isRedirect());
 	}
 
 	public function testGetRedirectTarget() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getRedirectTarget(), '');
+		$this->assertEquals('', $reply->getRedirectTarget());
 	}
 
 	public function testSetRedirectTarget() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setRedirectTarget('/path/to/resource'), $reply);
-		$this->assertEquals($reply->getRedirectTarget(), '/path/to/resource');
+		$this->assertSame($reply, $reply->setRedirectTarget('/path/to/resource'));
+		$this->assertEquals('/path/to/resource', $reply->getRedirectTarget());
 	}
 
 	public function testHasHeader() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
 		$this->assertTrue($reply->hasHeader('Content-Type'));
 		$this->assertFalse($reply->hasHeader('Location'));
@@ -142,99 +208,65 @@ extends TestCase
 	}
 
 	public function testGetHeader() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getHeader('Content-Type'), 'text/plain;charset=utf-8');
-		$this->assertEquals($reply->getHeader('Location'), false);
-		$this->assertEquals($reply->getHeader('Header-1'), false);
+		$this->assertEquals('text/plain;charset=iso-8859-1', $reply->getHeader('Content-Type'));
 	}
 
 	public function testSetHeader() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setHeader('Content-Type', 'text/html;charset=iso-8859-1'), $reply);
-		$this->assertEquals($reply->getHeader('Content-Type'), 'text/html;charset=iso-8859-1');
-		$this->assertEquals($reply->getMime(), 'text/html');
-		$this->assertEquals($reply->getEncoding(), 'iso-8859-1');
+		$this->assertSame($reply, $reply->setHeader(IHttpMessage::HEADER_CONTENT_TYPE, 'text/html;charset=utf-8'));
+		$this->assertEquals('text/html;charset=utf-8', $reply->getHeader(IHttpMessage::HEADER_CONTENT_TYPE));
+		$this->assertEquals('text/html', $reply->getMime());
+		$this->assertEquals('utf-8', $reply->getEncoding());
 
-		$this->assertEquals($reply->setHeader('Location', '/path/to/resource'), $reply);
-		$this->assertEquals($reply->getHeader('Location'), '/path/to/resource');
-		$this->assertEquals($reply->getRedirectTarget(), '/path/to/resource');
+		$this->assertSame($reply, $reply->setHeader(IHttpMessage::HEADER_LOCATION, '/path/to/resource'));
+		$this->assertEquals('/path/to/resource', $reply->getHeader(IHttpMessage::HEADER_LOCATION));
+		$this->assertEquals('/path/to/resource', $reply->getRedirectTarget());
 
-		$this->assertEquals($reply->setHeader('Header-1', 'foo'), $reply);
+		$this->assertSame($reply, $reply->setHeader('Header-1', 'foo'));
 		$this->assertTrue($reply->hasHeader('Header-1'));
-		$this->assertEquals($reply->getHeader('Header-1'), 'foo');
+		$this->assertEquals('foo', $reply->getHeader('Header-1'));
 	}
 
 	public function testResetHeader() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$reply->setHeader('Content-Type', 'text/html;charset=iso-8859-1');
-		$this->assertEquals($reply->resetHeader('Content-Type'), $reply);
-		$this->assertEquals($reply->getMime(), 'text/plain');
-		$this->assertEquals($reply->getEncoding(), 'utf-8');
-		$this->assertEquals($reply->getHeader('Content-Type'), 'text/plain;charset=utf-8');
-		$this->assertTrue($reply->hasHeader('Content-Type'));
+		$this->assertSame($reply, $reply->setHeader(IHttpMessage::HEADER_CONTENT_TYPE, 'text/html;charset=iso-8859-1'));
+		$this->assertSame($reply, $reply->resetHeader(IHttpMessage::HEADER_CONTENT_TYPE));
+		$this->assertFalse($reply->hasHeader(IHttpMessage::HEADER_CONTENT_TYPE));
+		$this->assertEquals('', $reply->getMime());
+		$this->assertEquals('', $reply->getEncoding());
 
-		$reply->setHeader('Location', '/path/to/resource');
-		$this->assertEquals($reply->resetHeader('Location'), $reply);
-		$this->assertEquals($reply->getRedirectTarget(), '');
-		$this->assertEquals($reply->getHeader('Location'), '');
-		$this->assertFalse($reply->hasHeader('Location'));
+		$this->assertSame($reply, $reply->setHeader(IHttpMessage::HEADER_LOCATION, '/path/to/resource'));
+		$this->assertSame($reply, $reply->resetHeader(IHttpMessage::HEADER_LOCATION));
+		$this->assertFalse($reply->hasHeader(IHttpMessage::HEADER_LOCATION));
+		$this->assertEquals('', $reply->getRedirectTarget());
 
-		$reply->setHeader('Header-1', 'foo');
-		$this->assertEquals($reply->resetHeader('Header-1'), $reply);
-		$this->assertEquals($reply->getHeader('Header-1'), '');
+		$this->assertSame($reply, $reply->setHeader('Header-1', 'foo'));
+		$this->assertSame($reply, $reply->resetHeader('Header-1'));
 		$this->assertFalse($reply->hasHeader('Header-1'));
 	}
 
-	public function testGetHeaders() {
-		$reply = new HttpReply($this->_driver);
-
-		$this->assertEquals($reply->getHeaders(), []);
-
-		$reply->setHeader('Header-1', 'foo');
-
-		$this->assertEquals($reply->getHeaders(), [
-			'Header-1' => 'foo'
-		]);
-
-		$reply->setHeader('Header-2', 'bar');
-
-		$this->assertEquals($reply->getHeaders(), [
-			'Header-1' => 'foo',
-			'Header-2' => 'bar'
-		]);
-	}
 
 	public function testGetBody() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->getBody(), '');
+		$this->assertEquals('200 - OK', $reply->getBody());
 	}
 
 	public function testSetBody() {
-		$reply = new HttpReply($this->_driver);
+		$reply = $this->_produceReply();
 
-		$this->assertEquals($reply->setBody('foo'), $reply);
-		$this->assertEquals($reply->getBody(), 'foo');
+		$this->assertSame($reply, $reply->setBody('foo'));
+		$this->assertEquals('foo', $reply->getBody());
 	}
 
 
 	public function testSend() {
-		$this->_driver->setReplyCallback(function(HttpReply& $reply) {
-			$this->assertEquals($reply->getCode(), '404');
-			$this->assertEquals($reply->getMime(), 'text/html');
-			$this->assertEquals($reply->getEncoding(), 'iso-8859-1');
-			$this->assertEquals($reply->getBody(), 'foo');
-		});
+		$reply = $this->_produceReply();
 
-		$this->_driver
-			->useReply()
-			->setCode('404')
-			->setMime('text/html')
-			->setEncoding('iso-8859-1')
-			->setBody('foo')
-			->send();
+		$this->assertTrue($reply->send());
 	}
 }
