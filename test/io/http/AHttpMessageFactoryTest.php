3a758300777d6050e8e23c1c6e3567d13b7d2e1b
chkt <ck@chkt.one>
2017-07-02 14:05:53 +0200

    Add AHttpMessageFactoryTest

---
+++ test/io/http/AHttpMessageFactoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace test\io\http;
+
+use PHPUnit\Framework\TestCase;
+
+use lola\io\http\IHttpMessageFactory;
+use lola\io\http\IHttpMessage;
+use lola\io\http\AHttpMessageFactory;
+use lola\io\http\HttpMessage;
+
+
+
+final class AHttpMessageFactoryTest
+extends TestCase
+{
+
+	private function _mockFactory() : IHttpMessageFactory {
+		$res = $this->getMockBuilder(AHttpMessageFactory::class)
+			->getMockForAbstractClass();
+
+		$res
+			->expects($this->any())
+			->method('_produceInstance')
+			->with()
+			->willReturnCallback(function() {
+				return new HttpMessage('foo', [], 'bar');
+			});
+
+		return $res;
+	}
+
+
+	public function testInheritance() {
+		$factory = $this->_mockFactory();
+
+		$this->assertInstanceOf(IHttpMessageFactory::class, $factory);
+	}
+
+	public function testGetMessage() {
+		$factory = $this->_mockFactory();
+		$message = $factory->getMessage();
+
+		$this->assertInstanceOf(IHttpMessage::class, $message);
+		$this->assertSame($message, $factory->getMessage());
+	}
+}
