9e02b1f1c9bad312ade1fd1eba9132f5c47cc70a
chkt <ck@chkt.one>
2017-07-27 17:46:54 +0200

    Add ITreeMutator tests

--- test/type/data/TreeDataTest.php
+++ test/type/data/TreeDataTest.php
@@ -27,6 +27,36 @@ extends TestCase
 		];
 	}
 
+	private function _produceMergeData() : array {
+		return [[
+			'propToProp' => 1,
+			'propToArray' => [
+				'a' => 2,
+				'b' => 3
+			],
+			'arrayToProp' => 4,
+			'arrayToArray' => [
+				'a' => 5,
+				'b' => 6
+			]
+		], [
+			'propToProp' => 7,
+			'propToArray' => 8,
+			'arrayToNokey' => [
+				'a' => 9,
+				'b' => 10
+			],
+			'arrayToProp' => [
+				'a' => 11,
+				'b' => 12
+			],
+			'arrayToArray' => [
+				'b' => 13,
+				'c' => 14
+			]
+		]];
+	}
+
 	private function _mockChildClass(array& $data = null) {
 		if (is_null($data)) $data = $this->_produceSampleData();
 
@@ -233,6 +263,123 @@ extends TestCase
 		$this->assertEquals($data->useItem('baz'), 4);
 		$this->assertEquals($data->setItem('bar.foo', 5), $data);
 		$this->assertEquals($data->useItem('bar.foo'), 5);
+	}
+
+
+	public function testFilter() {
+		$data = [
+			'foo' => 10,
+			'bar' => [
+				'foo' => 20,
+				'bar' => [
+					'foo' => 30,
+					'baz' => 3
+				],
+				'baz' => 2
+			],
+			'baz' => 1,
+			'quux' => [
+				'baz' => false,
+				'quux' => true
+			]
+		];
+		$source = $this->_produceData($data);
+		$target = $this->_produceData();
+
+		$this->assertEquals(1, $target->useItem('foo'));
+		$this->assertSame($target, $target->filter($source, [
+			'foo',
+			'bar.bar.foo',
+			'bar.bar.baz',
+			'quux.baz',
+			'quux'
+		]));
+
+		$this->assertEquals(10, $target->useItem('foo'));
+		$this->assertFalse($target->hasKey('bar.foo'));
+		$this->assertFalse($target->hasKey('bar.baz'));
+		$this->assertEquals(30, $target->useItem('bar.bar.foo'));
+		$this->assertEquals(3, $target->useItem('bar.bar.baz'));
+		$this->assertFalse($target->hasKey('baz'));
+		$this->assertFalse($target->useItem('quux.baz'));
+		$this->assertTrue($target->useItem('quux.quux'));
+	}
+
+	public function testFilterEq() {
+		$data = [
+			'foo' => 10,
+			'bar' => [
+				'foo' => 20,
+				'bar' => [
+					'foo' => 30,
+					'baz' => 3
+				],
+				'baz' => 2
+			],
+			'baz' => 1,
+			'quux' => [
+				'baz' => false,
+				'quux' => true
+			]
+		];
+		$ins = $this->_produceData($data);
+
+		$this->assertEquals(10, $ins->useItem('foo'));
+		$this->assertSame($ins, $ins->filterEq([
+			'bar.bar.foo',
+			'bar.bar.baz',
+			'quux.quux',
+			'quux'
+		]));
+
+		$this->assertFalse($ins->hasKey('foo'));
+		$this->assertFalse($ins->hasKey('baz'));
+		$this->assertFalse($ins->hasKey('bar.foo'));
+		$this->assertFalse($ins->hasKey('bar.baz'));
+		$this->assertEquals(30, $ins->useItem('bar.bar.foo'));
+		$this->assertEquals(3, $ins->useItem('bar.bar.baz'));
+		$this->assertFalse($ins->useItem('quux.baz'));
+		$this->assertTrue($ins->useItem('quux.quux'));
+	}
+
+
+	public function testMerge() {
+		list($a, $b) = $this->_produceMergeData();
+
+		$sourceA = $this->_produceData($a);
+		$sourceB = $this->_produceData($b);
+		$target = $this->_produceData();
+
+		$this->assertEquals(1, $target->useItem('foo'));
+		$this->assertSame($target, $target->merge($sourceA, $sourceB));
+		$this->assertFalse($target->hasKey('foo'));
+		$this->assertEquals(7, $target->useItem('propToProp'));
+		$this->assertEquals(8, $target->useItem('propToArray'));
+		$this->assertEquals(9, $target->useItem('arrayToNokey.a'));
+		$this->assertEquals(10, $target->useItem('arrayToNokey.b'));
+		$this->assertEquals(11, $target->useItem('arrayToProp.a'));
+		$this->assertEquals(12, $target->useItem('arrayToProp.b'));
+		$this->assertEquals(5, $target->useItem('arrayToArray.a'));
+		$this->assertEquals(13, $target->useItem('arrayToArray.b'));
+		$this->assertEquals(14, $target->useItem('arrayToArray.c'));
+	}
+
+	public function testMergeEq() {
+		list($a, $b) = $this->_produceMergeData();
+
+		$source = $this->_produceData($b);
+		$target = $this->_produceData($a);
+
+		$this->assertSame($target, $target->mergeEq($source));
+		$this->assertEquals(7, $target->useItem('propToProp'));
+		$this->assertEquals(8, $target->useItem('propToArray'));
+		$this->assertEquals(9, $target->useItem('arrayToNokey.a'));
+		$this->assertEquals(10, $target->useItem('arrayToNokey.b'));
+		$this->assertEquals(11, $target->useItem('arrayToProp.a'));
+		$this->assertEquals(12, $target->useItem('arrayToProp.b'));
+		$this->assertEquals(5, $target->useItem('arrayToArray.a'));
+		$this->assertEquals(13, $target->useItem('arrayToArray.b'));
+		$this->assertEquals(14, $target->useItem('arrayToArray.c'));
 	}
 
 
