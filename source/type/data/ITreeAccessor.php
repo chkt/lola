0e2c411cf6e54bf87de1161cff033120483887b2
chkt <ck@chkt.one>
2017-08-06 10:50:23 +0200

    Remove getBranch(), setBranch(); Change to inherit IKeyAccessor

--- source/type/data/ITreeAccessor.php
+++ source/type/data/ITreeAccessor.php
@@ -2,21 +2,15 @@
 
 namespace lola\type\data;
 
-use lola\type\data\IKeyMutator;
 use lola\type\IProjectable;
 
 
 
 interface ITreeAccessor
-extends IKeyMutator, IProjectable
+extends IKeyAccessor, IProjectable
 {
 
 	public function isBranch(string $key) : bool;
 
 	public function isLeaf(string $key) : bool;
-
-
-	public function getBranch(string $key) : ITreeAccessor;
-
-	public function setBranch(string $key, ITreeAccessor $branch) : ITreeAccessor;
 }
