bbbe2289097099317779b6c35b880ba4d466d843
chkt <ck@chkt.one>
2017-08-06 10:51:36 +0200

    Add ->select(), ->selectSelf(), ->insert() Rename ->filterEq() to ->filterSelf()

--- source/type/data/ITreeMutator.php
+++ source/type/data/ITreeMutator.php
@@ -8,12 +8,20 @@ interface ITreeMutator
 extends ITreeAccessor
 {
 
-	public function filter(ITreeAccessor $tree, array $keys) : ITreeMutator;
-
-	public function filterEq(array $keys) : ITreeMutator;
-
-
 	public function merge(ITreeAccessor $a, ITreeAccessor $b) : ITreeMutator;
 
 	public function mergeEq(ITreeAccessor $b) : ITreeMutator;
+
+
+	public function filter(ITreeAccessor $tree, array $keys) : ITreeMutator;
+
+	public function filterSelf(array $keys) : ITreeMutator;
+
+
+	public function select(ITreeAccessor $tree, string $key) : ITreeMutator;
+
+	public function selectSelf(string $key) : ITreeMutator;
+
+
+	public function insert(ITreeAccessor $tree, string $key) : ITreeMutator;
 }
