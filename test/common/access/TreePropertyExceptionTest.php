47250f7992f2707518f1968bd457f71666d29eff
chkt <ck@chkt.one>
2017-09-21 11:46:53 +0200

    Add TreePropertyExceptionTest

---
+++ test/common/access/TreePropertyExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace test\common\access;
+
+use PHPUnit\Framework\TestCase;
+
+use lola\common\access\ATreeAccessorException;
+use lola\common\access\TreePropertyException;
+
+
+
+final class TreePropertyExceptionTest
+extends TestCase
+{
+
+	private function _produceException(& $item = null, array $resolved = [], array $missing = []) : TreePropertyException {
+		return new TreePropertyException($item, $resolved, $missing);
+	}
+
+	public function testInheritance() {
+		$ins = $this->_produceException();
+
+		$this->assertInstanceOf(ATreeAccessorException::class, $ins);
+	}
+
+	public function testGetMessage() {
+		$null = null;
+		$ins = $this->_produceException($null, ['foo', 'bar'], ['baz', 'quux']);
+
+		$this->assertEquals('ACC no property "foo.bar!baz.quux"', $ins->getMessage());
+	}
+}
