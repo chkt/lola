4bdc8c3240f7bbbfce4ade26425d6a15d3e01c67
chkt <ck@chkt.one>
2017-09-21 11:45:35 +0200

    Add ATreeAccessorExceptionTest

---
+++ test/common/access/ATreeAccessorExceptionTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace test\common\access;
+
+use PHPUnit\Framework\TestCase;
+
+use eve\access\IAccessorException;
+use lola\common\access\ITreeAccessorException;
+use lola\common\access\ATreeAccessorException;
+
+
+
+final class ATreeAccessorExceptionTest
+extends TestCase
+{
+
+	private function _mockException(& $item = null, array $resolved = [], array $missing = []) {
+		$ins = $this
+			->getMockBuilder(ATreeAccessorException::class)
+			->setConstructorArgs([ & $item, $resolved, $missing ])
+			->getMockForAbstractClass();
+
+		return $ins;
+	}
+
+
+	public function testInheritance() {
+		$ex = $this->_mockException();
+
+		$this->assertInstanceOf(IAccessorException::class, $ex);
+		$this->assertInstanceOf(ITreeAccessorException::class, $ex);
+	}
+
+
+	public function testGetMessage() {
+		$null = null;
+		$ins = $this
+			->getMockBuilder(ATreeAccessorException::class)
+			->disableOriginalConstructor()
+			->getMockForAbstractClass();
+
+		$ins
+			->expects($this->once())
+			->method('_produceMessage')
+			->with()
+			->willReturn('%s-%s');
+
+		$ins->__construct($null, ['foo.bar'], ['baz.quux']);
+
+		$this->assertEquals('foo.bar-baz.quux', $ins->getMessage());
+	}
+
+
+	public function testUseResolvedItem() {
+		$item = [ 'foo' => 1 ];
+		$ex = $this->_mockException($item);
+
+		$resolved =& $ex->useResolvedItem();
+
+		$this->assertSame($item, $resolved);
+
+		$item['bar'] = 2;
+		$resolved['baz'] = 3;
+
+		$this->assertSame($item, $resolved);
+	}
+
+	public function testGetKey() {
+		$null = null;
+		$ex = $this->_mockException($null, ['foo', 'bar'], ['baz', 'quux']);
+
+		$this->assertEquals('foo.bar.baz.quux', $ex->getKey());
+	}
+
+
+	public function testGetResolvedKeySegment() {
+		$null = null;
+		$ex = $this->_mockException($null, ['foo', 'bar'], []);
+
+		$this->assertEquals('foo.bar', $ex->getResolvedKeySegment());
+	}
+
+	public function testGetResolvedPath() {
+		$null = null;
+		$path = ['foo', 'bar'];
+		$ex = $this->_mockException($null, $path, []);
+
+		$this->assertSame($path, $ex->getResolvedPath());
+	}
+
+
+	public function testGetMissingKey() {
+		$null = null;
+		$ex = $this->_mockException($null, [], ['baz', 'quux']);
+
+		$this->assertEquals('baz.quux', $ex->getMissingKeySegment());
+	}
+
+	public function testGetMissingPath() {
+		$null = null;
+		$path = ['baz', 'quux'];
+		$ex = $this->_mockException($null, [], $path);
+
+		$this->assertSame($path, $ex->getMissingPath());
+	}
+}
