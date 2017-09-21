1445f6dab48c49ddaddde44e95ed35abf224137e
chkt <ck@chkt.one>
2017-09-21 11:49:26 +0200

    Add TreeAccessorFactoryTest

---
+++ test/common/access/TreeAccessorFactoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace test\common\access;
+
+use PHPUnit\Framework\TestCase;
+
+use eve\common\ISimpleFactory;
+use eve\factory\ICoreFactory;
+use lola\common\access\TreeAccessor;
+use lola\common\access\TreeAccessorFactory;
+
+
+
+final class TreeAccessorFactoryTest
+extends TestCase
+{
+
+	private function _mockCoreFactory() : ICoreFactory {
+		$ins = $this
+			->getMockBuilder(ICoreFactory::class)
+			->getMock();
+
+		$ins
+			->expects($this->any())
+			->method('newInstance')
+			->with(
+				$this->equalTo(TreeAccessor::class),
+				$this->isType('array')
+			)
+			->willReturnCallback(function(string $qname, array $opts = []) {
+				return new $qname(...$opts);
+			});
+
+		return $ins;
+	}
+
+	private function _produceFactory(ICoreFactory $coreFactory = null) : TreeAccessorFactory {
+		if (is_null($coreFactory)) $coreFactory = $this->_mockCoreFactory();
+
+		return new TreeAccessorFactory($coreFactory);
+	}
+
+
+	public function testInheritance() {
+		$ins = $this->_produceFactory();
+
+		$this->assertInstanceOf(ISimpleFactory::class, $ins);
+	}
+
+	public function testProduce() {
+		$fab = $this->_produceFactory();
+
+		$data = [
+			'foo' => 1,
+			'bar' => 2
+		];
+
+		$ins = $fab->produce($data);
+
+		$this->assertInstanceOf(TreeAccessor::class, $ins);
+		$this->assertEquals(1, $ins->getItem('foo'));
+		$this->assertEquals(2, $ins->getItem('bar'));
+
+		$data['foo'] = 3;
+
+		$this->assertEquals(3, $ins->getItem('foo'));
+	}
+}
