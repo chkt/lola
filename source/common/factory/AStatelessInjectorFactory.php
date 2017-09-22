2fe4ab64d76a994bccf0129aa585f8f4b28f43c4
chkt <ck@chkt.one>
2017-09-22 18:04:04 +0200

    Add AStatelessInjectorFactory

---
+++ source/common/factory/AStatelessInjectorFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace lola\common\factory;
+
+use eve\common\IProviderFactory;
+use eve\access\ITraversableAccessor;
+use eve\inject\IInjectableIdentity;
+use eve\inject\IInjectableFactory;
+
+
+abstract class AStatelessInjectorFactory
+implements IInjectableFactory
+{
+
+	static public function getInstanceIdentity(ITraversableAccessor $config) : string {
+		return IInjectableIdentity::IDENTITY_SINGLE;
+	}
+
+
+
+	private $_config;
+
+
+	public function __construct() {
+		$this->_config = null;
+	}
+
+
+	abstract protected function _produceInstance(ITraversableAccessor $config);
+
+
+	public function setConfig(ITraversableAccessor $config) : IProviderFactory {
+		$this->_config = $config;
+
+		return $this;
+	}
+
+	public function getInstance() {
+		if (is_null($this->_config)) throw new \ErrorException();
+
+		return $this->_produceInstance($this->_config);
+	}
+
+
+	public function produce(ITraversableAccessor $config) {
+		return $this
+			->setConfig($config)
+			->getInstance();
+	}
+}
