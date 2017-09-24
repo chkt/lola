efef5a5a3021aede456abe82c145f720561852e4
chkt <ck@chkt.one>
2017-09-24 22:11:47 +0200

    Add AServiceTest

---
+++ test/service/AServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace test\service;
+
+
+use PHPUnit\Framework\TestCase;
+
+use eve\access\TraversableAccessor;
+use eve\inject\IInjectable;
+use eve\inject\IInjectableIdentity;
+use lola\service\IService;
+use lola\service\AService;
+
+
+
+final class AServiceTest
+extends TestCase
+{
+
+	private function _mockService() {
+		$service = $this
+			->getMockBuilder(AService::class)
+			->getMockForAbstractClass();
+
+		return $service;
+	}
+
+
+	private function _produceAccessor(array& $data = []) : TraversableAccessor {
+		return new TraversableAccessor($data);
+	}
+
+
+	public function testInheritance() {
+		$service = $this->_mockService();
+
+		$this->assertInstanceOf(IService::class, $service);
+		$this->assertInstanceOf(IInjectableIdentity::class, $service);
+		$this->assertInstanceOf(IInjectable::class, $service);
+	}
+
+	public function testDependencyConfig() {
+		$this->assertEquals([], AService::getDependencyConfig($this->_produceAccessor()));
+	}
+
+	public function testInstanceIdentity() {
+		$this->assertEquals(IInjectableIdentity::IDENTITY_SINGLE, AService::getInstanceIdentity($this->_produceAccessor()));
+	}
+}
