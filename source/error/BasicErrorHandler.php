c6eb24689cf661f039f878d86274162c3721cc89
chkt <ck@chkt.one>
2017-10-09 22:42:46 +0200

    Refactored ErrorHandler to BasicErrorHandler

---
+++ source/error/BasicErrorHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace lola\error;
+
+use eve\access\ITraversableAccessor;
+use lola\log\ILogger;
+
+
+
+final class BasicErrorHandler
+implements IErrorHandler
+{
+
+	static public function getDependencyConfig(ITraversableAccessor $config) : array {
+		return [ 'environment:log' ];
+	}
+
+
+
+	private $_logger;
+
+
+	public function __construct(ILogger $logger) {
+		$this->_logger = $logger;
+	}
+
+
+	public function handleException(\Throwable $ex) {
+		if (!($ex instanceof NativeShutdownException)) $this->_logger->logException($ex);
+	}
+}
