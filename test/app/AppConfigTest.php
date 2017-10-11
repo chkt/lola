c409c0ce23b33b9016b355d0f08442d2a43fef3c
chkt <ck@chkt.one>
2017-10-11 06:42:26 +0200

    Add AppConfigTest

---
+++ test/app/AppConfigTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace test\app;
+
+use PHPUnit\Framework\TestCase;
+use phpmock\phpunit\PHPMock;
+
+use eve\access\IKeyAccessor;
+use eve\access\IItemAccessor;
+use lola\common\IComponentConfig;
+use lola\common\access\TreePropertyException;
+use lola\common\access\TreeAccessor;
+use lola\app\AppConfig;
+
+
+
+final class AppConfigTest
+extends TestCase
+{
+
+	use PHPMock;
+
+
+	private function _mockFilterInput() {
+		$this
+			->getFunctionMock('\\lola\\app', 'filter_input')
+			->expects($this->any())
+			->with(
+				$this->equalTo(INPUT_SERVER),
+				$this->equalTo('SCRIPT_FILENAME')
+			)
+			->willReturnCallback(function(string $type, string $name) : string {
+				switch ($name) {
+					case 'SCRIPT_FILENAME' : return '/foo/bar/file/something.php';
+					default : throw new \ErrorException();
+				}
+			});
+	}
+
+
+	private function _produceConfig(array $data = []) {
+		return new AppConfig($data);
+	}
+
+
+	public function testInheritance() {
+		$config = $this->_produceConfig();
+
+		$this->assertInstanceOf(TreeAccessor::class, $config);
+		$this->assertInstanceOf(IComponentConfig::class, $config);
+		$this->assertInstanceOf(IItemAccessor::class, $config);
+		$this->assertInstanceOf(IKeyAccessor::class, $config);
+	}
+
+	public function testRootPath() {
+		$this->_mockFilterInput();
+		$config = $this->_produceConfig();
+
+		$this->assertEquals('/foo/bar', $config->getItem('rootPath'));
+	}
+
+	public function testVerbosity() {
+		$config = $this->_produceConfig();
+
+		$this->assertEquals(0, $config->getItem('verbosity'));
+	}
+
+	public function testgetItem_noProperty() {
+		$config = $this->_produceConfig();
+
+		$this->expectException(TreePropertyException::class);
+		$this->expectExceptionMessage('foo');
+
+		$config->getItem('ACC no property "!foo"');
+	}
+}
