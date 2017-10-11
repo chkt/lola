c60820d42d90d367ce43d6072352291c1b087000
chkt <ck@chkt.one>
2017-10-11 06:42:56 +0200

    Add AppConfig

---
+++ source/app/AppConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace lola\app;
+
+use lola\common\IComponentConfig;
+use lola\common\access\TreeAccessor;
+use lola\common\access\TreePropertyException;
+
+
+
+class AppConfig
+extends TreeAccessor
+implements IComponentConfig
+{
+
+	const CONFIG_ROOT_PATH = 'rootPath';
+	const CONFIG_VERBOSITY = 'verbosity';
+
+
+
+	public function __construct(array $data = []) {
+		parent::__construct($data);
+	}
+
+
+	private function _getRootPath() {
+		$segs = explode(DIRECTORY_SEPARATOR, filter_input(INPUT_SERVER, 'SCRIPT_FILENAME'));
+
+		return implode(DIRECTORY_SEPARATOR, array_slice($segs, 0, count($segs) - 2));
+	}
+
+
+	protected function _handlePropertyException(TreePropertyException $ex) {
+		$data =& $ex->useResolvedItem();
+		$key = $ex->getKey();
+
+		if ($key === 'verbosity') $data['verbosity'] = 0;
+		else if ($key === 'rootPath') $data['rootPath'] = $this->_getRootPath();
+		else return false;
+
+		return true;
+	}
+}
