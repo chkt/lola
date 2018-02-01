591c49a2baa4d5c72b408e48c8ecd72b66ec60e2
chkt <ck@chkt.one>
2018-02-01 14:24:19 +0800

    Add ->_produceEntityParser()

--- source/app/CoreProviderFactory.php
+++ source/app/CoreProviderFactory.php
@@ -5,14 +5,21 @@ namespace lola\app;
 use eve\common\access\ITraversableAccessor;
 use eve\common\factory\ICoreFactory;
 use eve\driver\IInjectorDriver;
+use eve\driver\InjectorDriverFactory;
+use eve\entity\IEntityParser;
+use lola\module\EntityParser;
 
 
 
 final class CoreProviderFactory
-extends \eve\driver\InjectorDriverFactory
+extends InjectorDriverFactory
 {
 
-	protected function _produceDriver(ICoreFactory $core, ITraversableAccessor $config, array & $dependencies) : IInjectorDriver {
-		return $core->newInstance(CoreProvider::class, [ & $dependencies ]);
+	protected function _produceDriver(ICoreFactory $base, ITraversableAccessor $config, array & $dependencies) : IInjectorDriver {
+		return $base->newInstance(CoreProvider::class, [ & $dependencies ]);
+	}
+
+	protected function _produceEntityParser(IInjectorDriver $driver, ITraversableAccessor $config) : IEntityParser {
+		return $driver->getCoreFactory()->newInstance(EntityParser::class);
 	}
 }
