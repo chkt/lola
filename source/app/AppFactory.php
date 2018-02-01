54a28796a82996dd52aeca43efe8f0eed04cc318
chkt <ck@chkt.one>
2018-02-01 14:25:49 +0800

    Add ->_produceNativeErrorSource(), cache key encoding through IKeyEncoder

--- source/app/AppFactory.php
+++ source/app/AppFactory.php
@@ -5,6 +5,9 @@ namespace lola\app;
 use eve\common\factory\ICoreFactory;
 use eve\common\factory\ASimpleFactory;
 use eve\inject\IInjectableIdentity;
+use lola\error\INativeErrorSource;
+use lola\error\NativeErrorSource;
+
 
 
 class AppFactory
@@ -13,35 +16,44 @@ extends ASimpleFactory
 
 	protected function _getConfigDefaults() : array {
 		return [
-			'entityParserName' => \lola\module\EntityParser::class,
 			'providers' => [
 				'core' => \lola\app\CoreProvider::class,
 				'environment' => \lola\provide\EnvironmentProvider::class,
 				'service' => \lola\service\ServiceProvider::class,
 				'controller' => \lola\ctrl\ControllerProvider::class
 			],
-			'global' => [],
-			'errorSourceName' => \lola\error\NativeErrorSource::class
+			'global' => []
 		];
 	}
 
 
-	protected function _produceInstance(ICoreFactory $core, array $config) {
-		$driverFactory = $core->newInstance(CoreProviderFactory::class, [ $core ]);
+	protected function _produceNativeErrorSource(IApp $app, array $config) : INativeErrorSource {
+		return $app
+			->getInjector()
+			->produce(NativeErrorSource::class);
+	}
+
+
+	protected function _produceInstance(ICoreFactory $base, array $config) {
+		$driverFactory = $base->newInstance(CoreProviderFactory::class, [ $base ]);
 		$driver = $driverFactory->produce($config);
+
+		$key = $driver
+			->getKeyEncoder()
+			->encode(CoreProvider::class, IInjectableIdentity::IDENTITY_SINGLE);
 
 		$driver
 			->getInstanceCache()
-			->setItem(CoreProvider::class . ':' . IInjectableIdentity::IDENTITY_SINGLE, $driver);
+			->setItem($key, $driver);
 
-		$injector = $driver->getInjector();
+		$app = $driver
+			->getInjector()
+				->produce(App::class, [
+				'driver' => $driver,
+				'component' => $base->newInstance(AppConfig::class, [ $config['global'] ])
+			]);
 
-		$app = $injector->produce(App::class, [
-			'driver' => $driver,
-			'component' => $core->newInstance(AppConfig::class, [ $config['global'] ])
-		]);
-
-		$injector->produce($config['errorSourceName']);
+		$this->_produceNativeErrorSource($app, $config);
 
 		return $app;
 	}
