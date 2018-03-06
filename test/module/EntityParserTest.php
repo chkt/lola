38515a5648be108cc6b2d591fba0fea14b1f3f61
chkt <ck@chkt.one>
2018-03-06 14:50:20 +0800

    Update testParse_nameConfig() to expect parsed query strings

--- test/module/EntityParserTest.php
+++ test/module/EntityParserTest.php
@@ -2,7 +2,9 @@
 
 use PHPUnit\Framework\TestCase;
 
-use eve\entity\IEntityParser;
+use eve\common\ITokenizer;
+use eve\entity\IEntityParser as IParentEntityParser;
+use lola\module\IEntityParser;
 use lola\module\EntityParser;
 
 
@@ -11,9 +13,27 @@ final class EntityParserTest
 extends TestCase
 {
 
+	private function _mockConfigParser(callable $fn = null) {
+		if (is_null($fn)) $fn = function(string $source) {
+			return [ $source ];
+		};
 
-	private function _produceParser() {
-		return new EntityParser();
+		$parser = $this
+			->getMockBuilder(ITokenizer::class)
+			->getMock();
+
+		$parser
+			->method('parse')
+			->with($this->isType('string'))
+			->willReturnCallback($fn);
+
+		return $parser;
+	}
+
+	private function _produceParser(ITokenizer $configParser = null) {
+		if (is_null($configParser)) $configParser = $this->_mockConfigParser();
+
+		return new EntityParser($configParser);
 	}
 
 
@@ -21,6 +41,8 @@ extends TestCase
 		$ins = $this->_produceParser();
 
 		$this->assertInstanceOf(IEntityParser::class, $ins);
+		$this->assertInstanceOf(IParentEntityParser::class, $ins);
+		$this->assertInstanceOf(ITokenizer::class, $ins);
 	}
 
 
@@ -73,34 +95,38 @@ extends TestCase
 	}
 
 	public function testParse_nameConfig() {
-		$ins = $this->_produceParser();
+		$configParser = $this->_mockConfigParser(function(string $source) {
+			return ['foo' => 'bar'];
+		});
+		$ins = $this->_produceParser($configParser);
+
 
 		$this->assertEquals([
 			EntityParser::COMPONENT_TYPE => 'foo',
 			EntityParser::COMPONENT_MODULE => 'bar',
 			EntityParser::COMPONENT_NAME => '/baz',
-			EntityParser::COMPONENT_CONFIG => 'id=quux'
+			EntityParser::COMPONENT_CONFIG => [ 'foo' => 'bar' ]
 		], $ins->parse('foo://bar/baz?id=quux', EntityParser::COMPONENT_NAME));
 
 		$this->assertEquals([
 			EntityParser::COMPONENT_TYPE => '',
 			EntityParser::COMPONENT_MODULE => '',
 			EntityParser::COMPONENT_NAME => '/baz',
-			EntityParser::COMPONENT_CONFIG => ''
+			EntityParser::COMPONENT_CONFIG => []
 		], $ins->parse('/baz', EntityParser::COMPONENT_NAME));
 
 		$this->assertEquals([
 			EntityParser::COMPONENT_TYPE => '',
 			EntityParser::COMPONENT_MODULE => '',
 			EntityParser::COMPONENT_NAME => 'baz',
-			EntityParser::COMPONENT_CONFIG => ''
+			EntityParser::COMPONENT_CONFIG => []
 		], $ins->parse('baz', EntityParser::COMPONENT_NAME));
 
 		$this->assertEquals([
 			EntityParser::COMPONENT_TYPE => '',
 			EntityParser::COMPONENT_MODULE => '',
 			EntityParser::COMPONENT_NAME => '',
-			EntityParser::COMPONENT_CONFIG => 'id=quux'
+			EntityParser::COMPONENT_CONFIG => [ 'foo' => 'bar' ]
 		], $ins->parse('?id=quux', EntityParser::COMPONENT_NAME));
 	}
 
