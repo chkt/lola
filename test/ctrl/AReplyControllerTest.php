6b7b9581801af6012d5149fa79fa9b60d8c710c3
chkt <ck@chkt.one>
2018-07-06 18:05:39 +0200

    Add ->testEnter()

--- test/ctrl/AReplyControllerTest.php
+++ test/ctrl/AReplyControllerTest.php
@@ -3,9 +3,9 @@
 namespace test\ctrl;
 
 use PHPUnit\Framework\TestCase;
-
 use eve\common\access\TraversableAccessor;
-use lola\ctrl\AController;
+use lola\ctrl\IControllerState;
+use lola\ctrl\ControllerTransform;
 use lola\ctrl\AReplyController;
 use test\io\http\MockDriver;
 
@@ -15,12 +15,21 @@ class AReplyControllerTest
 extends TestCase
 {
 
-	private function _mockController() {
+	private function _mockControllerState() {
+		$res = $this
+			->getMockBuilder(IControllerState::class)
+			->getMock();
+
+		return $res;
+	}
+
+	private function _mockController(array $actions = []) {
 		$driver = new MockDriver();
 
 		return $this
 			->getMockBuilder(AReplyController::class)
-			->setConstructorArgs([ & $driver ])
+			->setConstructorArgs([& $driver])
+			->setMethods($actions)
 			->getMockForAbstractClass();
 	}
 
@@ -33,8 +42,9 @@ extends TestCase
 	public function testInheritance() {
 		$ctrl = $this->_mockController();
 
-		$this->assertInstanceOf(AController::class, $ctrl);
+		$this->assertInstanceOf(\lola\ctrl\AController::class, $ctrl);
 	}
+
 
 	public function testDependencyConfig() {
 		$this->assertEquals([ 'environment:http'], AReplyController::getDependencyConfig($this->_produceAccessor()));
@@ -103,5 +113,52 @@ extends TestCase
 
 		$this->assertEquals($controller, $controller->setReplyTransform($transform));
 		$this->assertEquals($transform, $controller->useReplyTransform());
+	}
+
+
+	public function testEnter() {
+		$state = $this->_mockControllerState();
+		$controller = $this->_mockController([ 'fooAction', 'useReply' ]);
+		$transform = $this
+			->getMockBuilder(ControllerTransform::class)
+			->setConstructorArgs([[
+				ControllerTransform::STEP_FIRST => [
+					'next' => [ ControllerTransform::STEP_SUCCESS => 'foo' ]
+				],
+				'foo' => [
+					'transform' => 'foo',
+					'next' => [ ControllerTransform::STEP_SUCCESS => ControllerTransform::STEP_END ]
+				]
+			]])
+			->setMethods([ 'fooStep' ])
+			->getMock();
+
+		$transform
+			->expects($this->exactly(2))
+			->method('fooStep')
+			->with($controller);
+
+		$reply = $this
+			->getMockBuilder(\lola\io\http\IHttpReply::class)
+			->getMock();
+
+		$reply
+			->expects($this->exactly(1))
+			->method('send');
+
+		$controller
+			->setRequestTransform($transform)
+			->setReplyTransform($transform);
+
+		$controller
+			->method('fooAction')
+			->with($this->equalTo($state))
+			->willReturn(null);
+
+		$controller
+			->method('useReply')
+			->willReturn($reply);
+
+		$controller->enter('foo', $state);
 	}
 }
