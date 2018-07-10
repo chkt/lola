d3f481bc2a237d13e304e0a5a0658bded1a7f387
chkt <ck@chkt.one>
2018-07-10 21:25:24 +0200

    Move method definitions from IRequest to IHttpRequest

--- source/io/IRequest.php
+++ source/io/IRequest.php
@@ -2,40 +2,9 @@
 
 namespace lola\io;
 
-use lola\io\IReply;
-use lola\io\IClient;
-
 
 
 interface IRequest
 {
 
-	public function& useReply() : IReply;
-
-	public function& useClient() : IClient;
-
-
-	public function getTime() : int;
-
-	public function setTime(int $time) : IRequest;
-
-
-	public function getProtocol() : string;
-
-	public function setProtocol(string $protocol) : IRequest;
-
-
-	public function getHostName() : string;
-
-	public function setHostName(string $hostName) : IRequest;
-
-
-	public function getPath() : string;
-
-	public function setPath(string $path) : IRequest;
-
-
-	public function& useQuery() : array;
-
-	public function setQuery(array $query) : IRequest;
 }
--- source/io/http/IHttpRequest.php
+++ source/io/http/IHttpRequest.php
@@ -3,7 +3,8 @@
 namespace lola\io\http;
 
 use lola\io\IRequest;
-use lola\io\http\IHttpCookies;
+use lola\io\IReply;
+use lola\io\IClient;
 use lola\io\http\payload\IHttpPayload;
 
 
@@ -12,14 +13,44 @@ interface IHttpRequest
 extends IRequest
 {
 
+	public function& useReply() : IReply;
+
+	public function& useClient() : IClient;
+
+
 	public function& usePayload() : IHttpPayload;
 
 	public function& useCookies() : IHttpCookies;
 
 
+	public function getTime() : int;
+
+	public function setTime(int $time) : IRequest;
+
+
+	public function getProtocol() : string;
+
+	public function setProtocol(string $protocol) : IRequest;
+
+
 	public function getMethod() : string;
 
 	public function setMethod(string $method) : IHttpRequest;
+
+
+	public function getHostName() : string;
+
+	public function setHostName(string $hostName) : IRequest;
+
+
+	public function getPath() : string;
+
+	public function setPath(string $path) : IRequest;
+
+
+	public function& useQuery() : array;
+
+	public function setQuery(array $query) : IRequest;
 
 
 	public function getMime() : string;
