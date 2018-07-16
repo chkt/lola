06d175b0600fd83c2a99ddd32fb7e567c6d5ab8f
chkt <ck@chkt.one>
2018-07-16 11:14:56 +0200

    Add test for configurable accessorFactory

--- test/app/CoreProviderFactoryTest.php
+++ test/app/CoreProviderFactoryTest.php
@@ -7,8 +7,8 @@ use PHPUnit\Framework\TestCase;
 use eve\common\factory\IBaseFactory;
 use eve\common\factory\ISimpleFactory;
 use eve\common\access\ITraversableAccessor;
-use eve\common\access\factory\TraversableAccessorFactory;
 use eve\common\assembly\IAssemblyHost;
+use eve\inject\IInjector;
 use eve\driver\IInjectorDriver;
 use lola\app\CoreProvider;
 use lola\app\CoreProviderFactory;
@@ -34,6 +34,70 @@ extends TestCase
 	}
 
 
+	private function _mockAccessorFactory(array $args) {
+		$access = $this->_mockInterface(ISimpleFactory::class, $args);
+
+		$access
+			->method('produce')
+			->with($this->isType('array'))
+			->willReturnCallback(function(array& $data) {
+				return $this->_mockInterface(ITraversableAccessor::class, [ & $data ]);
+			});
+
+		return $access;
+	}
+
+	private function _mockDriverAssembly(array $args) {
+		$assembly = $this->_mockInterface(IAssemblyHost::class, $args);
+
+		$assembly
+			->method('getItem')
+			->with($this->equalTo('baseFactory'))
+			->willReturn($assembly->p0);
+
+		return $assembly;
+	}
+
+	private function _mockBaseFactory(callable $mergeMethod = null) {
+		if (is_null($mergeMethod)) $mergeMethod = function(array $a, array $b) : array {
+			return [];
+		};
+
+		$base = $this->_mockInterface(IBaseFactory::class);
+
+		$base
+			->method('callMethod')
+			->with(
+				$this->equalTo(\eve\common\base\ArrayOperation::class),
+				$this->equalTo('merge'),
+				$this->logicalAnd(
+					$this->isType('array'),
+					$this->countOf(2)
+				)
+			)
+			->willReturnCallback(function(string $qname, string $method, array $args) use ($mergeMethod) {
+				return $mergeMethod(...$args);
+			});
+
+		$base
+			->method('produce')
+			->with(
+				$this->isType('string'),
+				$this->logicalOr(
+					$this->isType('array'),
+					$this->isNull()
+				)
+			)
+			->willReturnCallback(function(string $qname, array $args) use ($base) {
+				if ($qname === CoreProviderAssembly::class) return $this->_mockDriverAssembly($args);
+				else if ($qname === 'foo') return $this->_mockAccessorFactory($args);
+				else if ($qname === CoreProvider::class) return $this->_mockInterface(IInjectorDriver::class, $args);
+				else $this->fail($qname);
+			});
+
+		return $base;
+	}
+
 	private function _produceProviderFactory(IBaseFactory $base = null) : CoreProviderFactory {
 		if (is_null($base)) $base = $this->_mockInterface(IBaseFactory::class);
 
@@ -49,61 +113,51 @@ extends TestCase
 
 
 	public function testProduce() {
-		$provider = $this->_mockInterface(IInjectorDriver::class);
-		$accessorFactory = $this->_mockInterface(ISimpleFactory::class);
+		$config = [ 'foo' => 'bar' ];
 
-		$accessorFactory
-			->method('produce')
-			->with($this->isType('array'))
-			->willReturnCallback(function(array& $data) {
-				return $this->_mockInterface(ITraversableAccessor::class, [ & $data ]);
-			});
+		$base = $this->_mockBaseFactory(function(array $a, array $b) use ($config) {
+			$this->assertEquals([
+				'resolvers' => [
+					IInjector::TYPE_INJECTOR => \eve\inject\resolve\HostResolver::class,
+					IInjector::TYPE_LOCATOR => \eve\inject\resolve\HostResolver::class,
+					IInjector::TYPE_ARGUMENT => \eve\inject\resolve\ArgumentResolver::class,
+					IInjector::TYPE_FACTORY => \eve\inject\resolve\FactoryResolver::class
+				],
+				'providers' => [],
+				'accessorFactory' => \eve\common\access\factory\TraversableAccessorFactory::class
+			], $a);
 
-		$assembly = $this->_mockInterface(IAssemblyHost::class);
+			$a['accessorFactory'] = 'foo';
 
-		$defaults = null;
-		$base = $this->_mockInterface(IBaseFactory::class);
+			$this->assertEquals($config, $b);
 
-		$base
-			->method('callMethod')
-			->with(
-				$this->equalTo(\eve\common\base\ArrayOperation::class),
-				$this->equalTo('merge'),
-				$this->logicalAnd(
-					$this->isType('array'),
-					$this->countOf(2)
-				)
-			)
-			->willReturnCallback(function(string $qname, string $method, array $args) use(& $defaults) {
-				$this->assertEquals($defaults, $args[0]);
-				$this->assertEquals([], $args[1]);
-
-				return $defaults;
-			});
-
-		$base
-			->method('produce')
-			->with($this->isType('string'))
-			->willReturnCallback(function(string $qname) use ($base, $accessorFactory, $assembly, $provider) {
-				if ($qname === CoreProviderAssembly::class) return $assembly;
-				else if ($qname === TraversableAccessorFactory::class) return $accessorFactory;
-				else if ($qname === CoreProvider::class) return $provider;
-				else $this->fail($qname);
-			});
-
-		$assembly
-			->method('getItem')
-			->with($this->equalTo('baseFactory'))
-			->willReturn($base);
-
+			return $a;
+		});
 		$factory = $this->_produceProviderFactory($base);
 
-		$method = new \ReflectionMethod($factory, '_getConfigDefaults');
-		$method->setAccessible(true);
-		$defaults = $method->invoke($factory);
+		$driver = $factory->produce($config);
 
-		$result = $factory->produce();
-
-		$this->assertSame($provider, $result);
+		$this->assertInstanceOf(IInjectorDriver::class, $driver);
+		$this->assertObjectHasAttribute('p0', $driver);
+		$this->assertInstanceOf(IAssemblyHost::class, $driver->p0);
+		$this->assertObjectHasAttribute('p0', $driver->p0);
+		$this->assertInstanceOf(IBaseFactory::class, $driver->p0->p0);
+		$this->assertObjectHasAttribute('p1', $driver->p0);
+		$this->assertInstanceOf(ISimpleFactory::class, $driver->p0->p1);
+		$this->assertObjectHasAttribute('p0', $driver->p0->p1);
+		$this->assertInstanceOf(IBaseFactory::class, $driver->p0->p1->p0);
+		$this->assertObjectHasAttribute('p2', $driver->p0);
+		$this->assertInstanceOf(ITraversableAccessor::class, $driver->p0->p2);
+		$this->assertObjectHasAttribute('p0', $driver->p0->p2);
+		$this->assertInternalType('array', $driver->p0->p2->p0);
+		$this->assertArrayHasKey('resolvers', $driver->p0->p2->p0);
+		$this->assertEquals([
+			'injector' => \eve\inject\resolve\HostResolver::class,
+			'locator' => \eve\inject\resolve\HostResolver::class,
+			'argument' => \eve\inject\resolve\ArgumentResolver::class,
+			'factory' => \eve\inject\resolve\FactoryResolver::class
+		], $driver->p0->p2->p0['resolvers']);
+		$this->assertArrayHasKey('providers', $driver->p0->p2->p0);
+		$this->assertEquals([], $driver->p0->p2->p0['providers']);
 	}
 }
