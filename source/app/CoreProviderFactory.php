1bfbf603e4ee6bfec848867dae30662f5bef5850
chkt <ck@chkt.one>
2018-07-16 11:15:45 +0200

    Add ->_produceAccessorFactory() and 'accessorFactory' config default

--- source/app/CoreProviderFactory.php
+++ source/app/CoreProviderFactory.php
@@ -15,6 +15,17 @@ final class CoreProviderFactory
 extends InjectorDriverFactory
 {
 
+	protected function _getConfigDefaults() : array {
+		return array_merge(parent::_getConfigDefaults(), [
+			'accessorFactory' => \eve\common\access\factory\TraversableAccessorFactory::class
+		]);
+	}
+
+
+	protected function _produceAccessorFactory(IBaseFactory $base, array $config) : ISimpleFactory {
+		return $base->produce($config['accessorFactory'], [ $base ]);
+	}
+
 	protected function _produceAssembly(IBaseFactory $base, ISimpleFactory $access, ITraversableAccessor $config) : IAssemblyHost {
 		return $base->produce(CoreProviderAssembly::class, [
 			$base,
